5100cf2a9588c2858f7242b3e1b0b1e5dbfa1246
matt werth <matt werth>
2016-08-16 09:47:14 +1000

    testing ip host

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 require 'rdbi-driver-odbc'
-dbh = RDBI.connect :ODBC, :db => "testroc", :user => "cassy", :password => "roc"
+dbh = RDBI.connect :ODBC, :db => "testroc", :user => "cassy", :password => "roc", :host => "192.168.10.72:3000"
 rs = dbh.execute "SELECT * FROM MY_TABLE"
 rs.as(:Struct).fetch(:first)
 end
