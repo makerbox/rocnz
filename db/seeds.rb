8224decbab8b50c5814f7e0bc52458e4dc437817
matt werth <matt werth>
2016-09-01 11:12:52 +1000

    erg

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,9 @@ end
 customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
 
 customers.first do |cust|
-puts cust
+cust.each do |c|
+	puts c
+end
 end
 
 # CUSTOMER ORDER AS SEPARATE SCAFFOLD - EACH ORDER TRANSLATED INTO KFI FILE? CAN WE INTERACT WITH THE DATABASE DIRECTLY WITHOUT KFI?
