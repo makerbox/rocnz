fc95ccfcc50256e3b1305f0479c63e6ad8281ba3
matt werth <matt werth>
2016-09-15 15:45:17 +1000

    attempting product ext CostCentre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915002909) do
+ActiveRecord::Schema.define(version: 20160915053925) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160915002909) do
     t.string   "group"
     t.string   "imageurl"
     t.integer  "qty"
+    t.string   "category"
   end
 
   create_table "quantities", force: :cascade do |t|
