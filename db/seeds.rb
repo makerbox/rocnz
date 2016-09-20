402bde880a4f7416239ce47a75841abc84ec5c38
matt werth <matt werth>
2016-09-20 11:55:49 +1000

    something fucked up

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,16 +50,17 @@ products.each do |p|
 			if @product.category != category || @product.code != p.Code || @product.description != p.Description || @product.group != p.ProductGroup || @product.price1 != p.SalesPrice1 || @product.price2 != p.SalesPrice2 || @product.price3 != p.SalesPrice3 || @product.price4 != p.SalesPrice4 || @product.price5 != p.SalesPrice5 || @product.rrp != p.SalesPrice6 || @product.qty != p.QtyInStock 
 				Product.find_by(code: p.Code).update(category: category, qty: p.QtyInStock, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
 			else
-				puts "product exactly the same - skipping"
+				puts "already in db, skipping product"
 			end
 		else #if the product doesn't already exist, let's make it
-			Product.create(category: category, qty: p.QtyInStock, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
+			p = Product.new(category: category, qty: p.QtyInStock, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
 			#upload image to cloudinary and store url in product.imageurl (images are stored in z:/attache/roc/images/product/*sku*.jpg)
 			filename = "Z:\\Attache\\Roc\\Images\\Product\\" + p.Code.strip + '.jpg'
 			if File.exist?(filename)
 				Cloudinary::Uploader.upload(filename, :public_id => p.Code.strip, :overwrite => true)
+				p.create
 			else
-				#image doesn't exist - perhaps create image attribute and set it to 'empty' if no file, or filename(minus path) if exists
+				"no image, skipping product"
 			end
 		end
 	end
