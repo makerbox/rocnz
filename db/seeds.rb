809cc751b49c8f5766fa4e638e17984bcdb35aea
matt werth <matt werth>
2016-09-22 13:18:32 +1000

    new arrivals in seeds - should be all good now

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,23 +46,23 @@ products.each do |p|
 				category = x.CostCentre
 			end
 		end
+				#now for the date of arrival
+				onsale = '' #set onsale just in case there isn't one, because the program will skip the next loop entirely if it doesn't exist
+				produdefdata.each do |pro| #for each of the rows in the product user defined data file
+					if pro.Code == p.Code #see if it is the right product
+						onsale = pro.DateFld #if it is the right record, then take it's date and put it in the onsale variable
+					end
+				end
 
 		if @product #if the product already exists, just update the details
-			if @product.category != category || @product.code != p.Code || @product.description != p.Description || @product.group != p.ProductGroup || @product.price1 != p.SalesPrice1 || @product.price2 != p.SalesPrice2 || @product.price3 != p.SalesPrice3 || @product.price4 != p.SalesPrice4 || @product.price5 != p.SalesPrice5 || @product.rrp != p.SalesPrice6 || @product.qty != p.QtyInStock 
+			if @product.new_date != onsale || @product.category != category || @product.code != p.Code || @product.description != p.Description || @product.group != p.ProductGroup || @product.price1 != p.SalesPrice1 || @product.price2 != p.SalesPrice2 || @product.price3 != p.SalesPrice3 || @product.price4 != p.SalesPrice4 || @product.price5 != p.SalesPrice5 || @product.rrp != p.SalesPrice6 || @product.qty != p.QtyInStock 
 				@thisproduct = Product.find_by(code: p.Code)
 				@pending_sold = 0 #set pending sold qty to zero
 				Order.where(approved: false).each do |order| #for each pending order
 					@pending_sold = @pending_sold + order.quantities.where(product_id: @thisproduct.id).sum(:qty) #find how many of this product are on it
 				end
 				current_quantity = p.QtyInStock - @pending_sold #qty is qty available minus pending orders
-				#now for the date of arrival
-				produdefdata.each do |pro| #for each of the rows in the product user defined data file
-					if pro.Code == p.Code #see if it is the right product
-						onsale = pro.DateFld #if it is the right record, then take it's date and put it in the onsale variable
-					else
-						onsale = nil #otherwise just make it nil (produdefdata only contains records that are filled in)
-					end
-				end
+
 				@thisproduct.update(new_date: onsale, category: category, qty: current_quantity, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
 			else
 				puts "already in db, skipping product"
@@ -73,11 +73,10 @@ products.each do |p|
 			if File.exist?(filename) # if there is an image, then create the product
 				Cloudinary::Uploader.upload(filename, :public_id => p.Code.strip, :overwrite => true)
 				#now for the date of arrival
+				onsale = '' #set onsale just in case there isn't one, because the program will skip the next loop entirely if it doesn't exist
 				produdefdata.each do |pro| #for each of the rows in the product user defined data file
 					if pro.Code == p.Code #see if it is the right product
 						onsale = pro.DateFld #if it is the right record, then take it's date and put it in the onsale variable
-					else
-						onsale = nil #otherwise just make it nil (produdefdata only contains records that are filled in)
 					end
 				end
 				Product.create(new_date: onsale, category: category, qty: p.QtyInStock, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
