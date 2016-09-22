08e208e5aebedcfb61d72b520df489b1457567aa
matt werth <matt werth>
2016-09-22 14:39:13 +1000

    new arrivals button and controller stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,7 +49,7 @@ class ProductsController < ApplicationController
         redirect_to home_index_path
       end
     elsif filter == 'new'
-      @products = Products.where(group: availgroups)
+      @products = Product.where(group: availgroups)
       cutoff = Date.today - 30.days
       @products = @products.where(new_date '>= ?', cutoff)
     else
