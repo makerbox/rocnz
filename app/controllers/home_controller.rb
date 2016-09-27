92047f7ee9117ebf99c4d3deefd34c257c535336
makerbox <mattwerth@mattwerth.com>
2016-09-27 16:33:22 +1000

    update to tester

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   	@notice = 'routed'
   	system "git pull"
   	@notice = @notice + ' pulled'
-  	system "PowerShell"
+  	# system "PowerShell"
   	system "echo testing print please show this to Matt | out-printer"
   	@notice = @notice + ' printed'
   	puts @notice
