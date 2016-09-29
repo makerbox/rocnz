143131be543164f94a77188ef585a21d6b7cfb04
matt werth <matt werth>
2016-09-29 11:20:39 +1000

    creating printable order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,7 +18,7 @@ def pull
       # @print += @order.total
       @order.quantities.each do |q|
         @print += q.qty.to_s
-        @print += q.product.name
+        # @print += q.product.name
         # @print += q.product.price
       end
     else
