7b54daea06e83c71eaf1fa4648558cb59802367e
matt werth <matt werth>
2016-09-29 11:43:42 +1000

    creating printable order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,14 +13,14 @@ def pull
 
   def test
   	if params[:order]
-      @order = Order.find_by(params[:order])
-      @print = "new order total:" + @order.quantities.count.to_s
+      @order = Order.find_by(id: params[:order])
+      @print = "order quantities count:" + @order.quantities.count.to_s
       # @print += @order.total
-      @order.quantities.each do |qqq|
+      # @order.quantities.each do |qqq|
         @print = @print + 'hi-'
         # @print += q.product.name
         # @print += q.product.price
-      end
+      # end
     else
       @order = "nothing came through - params empty"
     end
