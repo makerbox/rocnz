e5ced93237a5a3d03524f6d708aeddbc21b5c75e
matt werth <matt werth>
2016-09-30 15:46:59 +1000

    testing productgroup_name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,8 +17,10 @@ class HomeController < ApplicationController
   def test #this has a view, so you can check variables and stuff
    @test = Product.all
    @test.each do |p|
-      cat = p.category.strip
-      p.update(category: cat)
+      if !p.category.nil?
+        cat = p.category.strip
+        p.update(category: cat)
+      end
     end
   end
 
