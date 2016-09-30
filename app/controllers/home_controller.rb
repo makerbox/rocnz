fc7d987bd46cbc22158070008da59907b30b9d37
matt werth <matt werth>
2016-09-30 11:01:27 +1000

    testing productgroup_name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,6 +15,12 @@ class HomeController < ApplicationController
   end
 
   def test #this has a view, so you can check variables and stuff
+    require 'rdbi-driver-odbc'
+    puts "connecting to database"
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+    Test.new = dbh.execute("SELECT * FROM productgroup_name").fetch(:all, :Struct)
+    @test = Test.all
+    
   end
 
 end
