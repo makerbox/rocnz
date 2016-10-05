13fc548a66593dccbd561257d45d32e25d50fd80
matt werth <matt werth>
2016-10-05 17:05:30 +1100

    just need to run clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -2,7 +2,5 @@ require 'clockwork'
 include Clockwork
 
 every(1.minutes, 'populate') { 
-     system("rake runner:testdb")
-puts 'bye'
-      # Product.delay.populate
+     system("RAILS_ENV=production bin/delayed_job start --exit-on-complete")
 }
