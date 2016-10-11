9431f0f6504984847ce3b6f962034b92882f58bf
matt werth <matt werth>
2016-10-11 13:18:08 +1100

    moved job to jobs folder

--- lib/clock.rb
+++ lib/clock.rb
@@ -2,11 +2,5 @@ require 'clockwork'
 require './config/boot'
 require './config/environment'
 
-module Clockwork
 
-  handler do |job|
-    puts "Running #{job}"
-  end
-
-  every(30.minutes, 'populate') { Delayed::Job.enqueue PopulateJob.new}
-end
+every(30.minutes, 'populate') { Delayed::Job.enqueue PopulateJob.new}
