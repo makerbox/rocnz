ba03892b7bc4a90366f46308c8f0e5f2b43a8657
matt werth <matt werth>
2016-10-12 15:14:00 +1100

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,9 @@ class Product < ActiveRecord::Base
 has_many :quantities
 has_many :orders, through: :quantities
 
-def discount
-	if Discount.where(product: self.group)
-		self.rrp = '*'
-	end
+
+if Discount.where(product: self.group)
+	self.rrp = '*'
 end
 
 end #end of class
