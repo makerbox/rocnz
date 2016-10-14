b6a18b5e58984eef85526c4bda1a2f6ab72147a3
matt werth <matt werth>
2016-10-14 12:12:21 +1100

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,12 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def discount
-	if Discount.find_by(product: 'G')
-		Discount.find_by(product: 'G').discount
-	else
-		15
-	end
+	self.group
+	# if Discount.find_by(product: 'G')
+	# 	Discount.find_by(product: 'G').discount
+	# else
+	# 	15
+	# end
 end
 
 end #end of class
