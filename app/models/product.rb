15558d1f758dc472cbf0e4cffa88e6cb55a26836
matt werth <matt werth>
2016-10-14 13:35:11 +1100

    updating accounts

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,12 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def discount
-	if disc = Discount.find_by(product: self.group)
+	if disc = Discount.find_by(producttype: 'group', product: self.group)
 		if (disc.customer == current_user.account.code) || (disc.customer == current_user.account.discount)
 			Discount.find_by(product: self.group).discount
 		end
 	end
-	if disc = Discount.find_by(product: self.code)
+	if disc = Discount.find_by(producttype: 'code', product: self.code)
 		if (disc.customer == current_user.account.code) || (disc.customer == current_user.account.discount)
 			Discount.find_by(product: self.code).discount
 		end
