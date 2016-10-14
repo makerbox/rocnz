b677cf81681e800cf057166ecf57f43a4fbe48dc
matt werth <matt werth>
2016-10-14 16:34:19 +1100

    stasthasdgh

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,7 @@ def discount(user)
 		Discount.find_by(producttype: 'code', product: self.code, customertype: 'group', customer: user.account.discount)
 	end
 	# user.account.discount
-	'current user' + user.account.code
+	'|' + user.account.code + '|' + self.group
 end
 
 end #end of class
