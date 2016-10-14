437b8e1d2b4a20c28bc4e2e0e7005aa8eb7c078f
matt werth <matt werth>
2016-10-14 16:55:24 +1100

    stasthasdgh

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,18 +3,7 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def discount(user)
-	if Discount.find_by(producttype: 'group', product: self.group, customertype: 'code', customer: user.account.code).exists?
-		Discount.find_by(producttype: 'group', product: self.group, customertype: 'code', customer: user.account.code).discount
-	end
-	if Discount.find_by(producttype: 'group', product: self.group, customertype: 'group', customer: user.account.discount).exists?
-		Discount.find_by(producttype: 'group', product: self.group, customertype: 'group', customer: user.account.discount).discount
-	end
-	if Discount.find_by(producttype: 'code', product: self.code, customertype: 'code', customer: user.account.code).exists?
-		Discount.find_by(producttype: 'code', product: self.code, customertype: 'code', customer: user.account.code).discount
-	end
-	if Discount.find_by(producttype: 'code', product: self.code, customertype: 'group', customer: user.account.discount).exists?
-		Discount.find_by(producttype: 'code', product: self.code, customertype: 'group', customer: user.account.discount).discount
-	end
+		Discount.find_by(producttype: 'group', product: self.group, customertype: 'code', customer: user.account.code).discount + Discount.find_by(producttype: 'group', product: self.group, customertype: 'group', customer: user.account.discount).discount + Discount.find_by(producttype: 'code', product: self.code, customertype: 'code', customer: user.account.code).discount + Discount.find_by(producttype: 'code', product: self.code, customertype: 'group', customer: user.account.discount).discount
 end
 
 end #end of class
