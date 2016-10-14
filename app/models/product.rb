e96fc86e97a544d69a1d646ea84c11e7c35d3025
matt werth <matt werth>
2016-10-14 14:06:06 +1100

    stasthasdgh

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,12 @@ has_many :orders, through: :quantities
 
 def discount(user)
 	if disc = Discount.find_by(producttype: 'group', product: self.group)
-		if (disc.customer == current_user.account.code) || (disc.customer == current_user.account.discount)
+		if (disc.customer == user.account.code) || (disc.customer == user.account.discount)
 			Discount.find_by(product: self.group).discount
 		end
 	end
 	if disc = Discount.find_by(producttype: 'code', product: self.code)
-		if (disc.customer == current_user.account.code) || (disc.customer == current_user.account.discount)
+		if (disc.customer == user.account.code) || (disc.customer == user.account.discount)
 			Discount.find_by(product: self.code).discount
 		end
 	end
