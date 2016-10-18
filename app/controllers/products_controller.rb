f20b6bcadfed4fe71884d95bca924e8e12afab12
matt werth <matt werth>
2016-10-18 16:26:50 +1100

    order as account

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -103,7 +103,8 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     if current_user.has_role? :admin
-      @order = current_user.mimic.account.user.orders.find_by(active:true)
+      # @order = current_user.mimic.account.user.orders.find_by(active:true)
+      @order = Order.first
     else
       @order = current_user.orders.find_by(active: true)
     end
