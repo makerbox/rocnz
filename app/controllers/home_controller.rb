8c58c3ffc724c77715863c9351336474b8b85d73
matt werth <matt werth>
2016-10-18 12:43:06 +1100

    order as account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
 
   def pull
       system "git pull"
-      system "rake db:migrate"
       # system "bundle"
       # system "rails restart -b 0.0.0.0"
       # system "rake jobs:work"
