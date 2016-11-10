0209429c43426d2808dde8318daf76586797727d
matt werth <matt werth>
2016-11-10 16:23:17 +1100

    asdg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -85,11 +85,11 @@ class HomeController < ApplicationController
         end
 
         if @product #if the product already exists, just update the details
-          if @product.category != category || @product.code != p.Code || @product.description != p.Description || @product.group != p.ProductGroup || @product.price1 != p.SalesPrice1 || @product.price2 != p.SalesPrice2 || @product.price3 != p.SalesPrice3 || @product.price4 != p.SalesPrice4 || @product.price5 != p.SalesPrice5 || @product.rrp != p.SalesPrice6 || @product.qty != p.QtyInStock 
-            Product.find_by(code: p.Code).update(category: category, qty: p.QtyInStock, code: p.Code, description: p.Description, group: p.ProductGroup, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
+          if @product.category != category.strip || @product.code != p.Code.strip || @product.description != p.Description || @product.group != p.ProductGroup.strip || @product.price1 != p.SalesPrice1 || @product.price2 != p.SalesPrice2 || @product.price3 != p.SalesPrice3 || @product.price4 != p.SalesPrice4 || @product.price5 != p.SalesPrice5 || @product.rrp != p.SalesPrice6 || @product.qty != p.QtyInStock 
+            Product.find_by(code: p.Code).update(category: category.strip, qty: p.QtyInStock, code: p.Code.strip, description: p.Description, group: p.ProductGroup.strip, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
           end
         else #if the product doesn't already exist, let's make it
-          Product.create(category: category, qty: p.QtyInStock, code: p.Code.strip, description: p.Description, group: p.ProductGroup.strip, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
+          Product.create(category: category.strip, qty: p.QtyInStock, code: p.Code.strip, description: p.Description, group: p.ProductGroup.strip, price1: p.SalesPrice1, price2: p.SalesPrice2, price3: p.SalesPrice3, price4: p.SalesPrice4, price5: p.SalesPrice5, rrp: p.SalesPrice6)
           #upload image to cloudinary and store url in product.imageurl (images are stored in z:/attache/roc/images/product/*sku*.jpg)
           filename = "Z:\\Attache\\Roc\\Images\\Product\\" + p.Code.strip + '.jpg'
           if File.exist?(filename)
