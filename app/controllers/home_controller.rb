61b2bfb3804704ec4899daa59aa94855f1a42758
matt werth <matt werth>
2016-12-09 10:53:36 +1100

    testing transactions with new odbc call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,7 @@ end
     # end
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     # @transactions = dbh.execute("SELECT * FROM product_transactions").fetch(:all, :Struct)
-    @transactions = dbh.execute("SELECT * FROM product_transactions WHERE Code='335B'").fetch(:all, :Struct)
+    @transactions = dbh.execute("SELECT * FROM product_transactions WHERE Code='335B' AND TranType='1'").fetch(:all, :Struct)
   end
 
   def seed
