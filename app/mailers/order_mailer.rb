d27d103ff517580ad35f58dcb414b682cb54597b
matt werth <matt werth>
2016-12-09 15:06:41 +1100

    mailer test

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,21 +4,6 @@ class OrderMailer < ApplicationMailer
 	def new_order(order)
 		mail(to: 'mattwerth@mattwerth.com', subject: 'Your order summary from RocCloudy')
 		@order = order
-		account = @order.user.account
   
-  
-		  @print = "YOUR ORDER FROM ROC CLOUDY WHOLESALE PORTAL ["+ Time.now.strftime("%d/%m/%Y || %r") +"] \r\n"
-		  @print += "------------------ WEB" + @order.id.to_s + " ----------------------- \r\n"
-
-		  @print += "------------------------------------------------------------------- \r\n"
-		  @print += account.street.to_s + ' | ' + account.suburb.to_s + ' | ' + account.phone.to_s + "\r\n"
-		  @print += "------------------------------------------------------------------- \r\n\n"
-		  @order.quantities.each do |q|
-		    
-		    @print += "~" + q.product.code.to_s + "\r\n[" + q.product.description.strip + ' x ' + q.qty.to_s + "\r\n\n"
-		  end
-		  @print += "-------------------------------------------------------------------\r\n"
-		  @print += "total: $" + @order.total.to_s + "\r\n"
-		  @print += "-------------------------------------------------------------------"
 	end
 end
