6781a231d28f97f5f545feb6eea876e1a2a51e00
matt werth <matt werth>
2016-12-09 14:39:11 +1100

    mailer test

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,7 +2,6 @@ class OrderMailer < ApplicationMailer
 		default from:'web@roccloudy.com'
 	
 	def new_order(order)
-		realemail = order.account.user.email
 		mail(to: 'mattwerth@mattwerth.com', subject: 'Your order summary from RocCloudy')
 	end
 end
