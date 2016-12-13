bf974fe02b353c12170e2378db5b58137397ac67
matt werth <matt werth>
2016-12-13 16:12:12 +1100

    testing new arrivals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,6 +34,7 @@ end
     # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     # @transactions = dbh.execute("SELECT * FROM product_transactions WHERE Code='#{product}' AND CustomerSupplier='SAMPLES' ").fetch(:all, :Struct)
     p = Product.first
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @info = dbh.execute("SELECT * FROM produdefdata WHERE Code='#{p.Code}' ").fetch(:all, :Struct)
 
   end
