b0deec1d3331044ae823cb952a757c388f8b178f
matt werth <matt werth>
2016-12-14 14:27:54 +1100

    seeeed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -92,7 +92,7 @@ class ProductsController < ApplicationController
     end
     if @products
       if params[:filter] == 'new'
-        @products = @products.where("new_date <= ?", Date.today - 30)
+        @products = @products.where("new_date <= ?", Date.today - 30.days)
       end
       @products = @products.where("qty > ?", 20)
       @products = @products.order(group: 'DESC').order(code: 'ASC')
