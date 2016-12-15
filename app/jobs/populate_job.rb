a1cf9bb5e9ae10e773d4fba266e3e0bebd74668f
matt werth <matt werth>
2016-12-15 14:23:31 +1100

    test

--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -18,43 +18,44 @@ class PopulateJob
     end
 
     discounts.each do |d|
-      percent = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
-      if percent > 0 # check there is an actual discount to apply
-        if d.CustomerType == 10 # affect discounts for customer codes
-          if d.ProductType == 10 # affect discounts for product codes
-            if Discount.find_by(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          elsif d.ProductType == 30 # affect discounts for product groups
-            if Discount.find_by(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          end
-        elsif d.CustomerType == 30 # affect discounts for customer groups
-          if d.ProductType == 10 # affect discounts for product codes
-            if Discount.find_by(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          elsif d.ProductType == 30 # affect discounts for product groups
-            if Discount.find_by(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          end
-        end
-      end
+      # percent = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
+      # if percent > 0 # check there is an actual discount to apply
+      #   if d.CustomerType == 10 # affect discounts for customer codes
+      #     if d.ProductType == 10 # affect discounts for product codes
+      #       if Discount.find_by(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
+      #         # do nothing
+      #       else
+      #         Discount.create(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
+      #       end
+      #     elsif d.ProductType == 30 # affect discounts for product groups
+      #       if Discount.find_by(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
+      #         # do nothing
+      #       else
+      #         Discount.create(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
+      #       end
+      #     end
+      #   elsif d.CustomerType == 30 # affect discounts for customer groups
+      #     if d.ProductType == 10 # affect discounts for product codes
+      #       if Discount.find_by(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
+      #         # do nothing
+      #       else
+      #         Discount.create(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
+      #       end
+      #     elsif d.ProductType == 30 # affect discounts for product groups
+      #       if Discount.find_by(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
+      #         # do nothing
+      #       else
+      #         Discount.create(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
+      #       end
+      #     end
+      #   end
+      # end
     end
 
     products.each do |p|
       @saledate = nil
       @saledate = dbh.execute("SELECT * FROM produdefdata WHERE Code='#{p.Code}' ").fetch(:all, :Struct) #{p.Code}
+      puts @saledate 
       @saledate = @saledate[0].DateFld
       @product = Product.find_by(code: p.Code)
       if p.Inactive == 0
