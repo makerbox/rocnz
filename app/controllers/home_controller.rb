a5967eae913bfb053a3d1ea40ce08580cdc0cd1a
matt werth <matt werth>
2016-12-15 17:05:42 +1100

    sale date test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,7 +34,8 @@ end
     @print = "<h1>Testing by Matt</h1><table><thead><tr><th>product code</th><th>price</th></tr></thead><tbody><tr><td>asdf</td><td>$123</td></tr></tbody></table><b>bold</b>"
     system "printhtml html='#{@print}'"
     system "printhtml html='testing, please show Matt'"
-    system 'printhtml html="this is a test, please show Matt"'
+    system ('printhtml html="this is a test, please show Matt"')
+    `printhtml html="this is a test, pleeeeeeaaaaasssee show <b>Matt</b>"`
 dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     # @transactions = dbh.execute("SELECT * FROM product_transactions WHERE Code='#{product}' AND CustomerSupplier='SAMPLES' ").fetch(:all, :Struct)
    dbhstring = "SELECT * FROM produdefdata"
