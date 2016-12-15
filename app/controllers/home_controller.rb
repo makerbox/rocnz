9e9930a5c43947adcb005aab15f68a9b6d855fee
matt werth <matt werth>
2016-12-15 12:51:33 +1100

    et

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,7 +32,7 @@ end
     # @product = Product.all
     # product = '335B'
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-    @dudefdata = dbh.execute("SELECT DateFld FROM produdefdata WHERE Code='348B' ").fetch(:DateFld, :Hash) #{p.Code}
+    @dudefdata = dbh.execute("SELECT DateFld FROM produdefdata WHERE Code='348B' ").fetch(:all, :Struct) #{p.Code}
     @dudefdata = @dudefdata
     # @transactions = dbh.execute("SELECT * FROM product_transactions WHERE Code='#{product}' AND CustomerSupplier='SAMPLES' ").fetch(:all, :Struct)
    
