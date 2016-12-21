46b554927ffec5355bc7054141e8a3e27ed350a2
matt werth <matt werth>
2016-12-21 17:24:43 +1100

    email fix

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,6 +5,6 @@ class OrderMailer < ApplicationMailer
 		@thisorder = order
 		@account = order.user.account
 		# mail(to: order.user.email, subject: 'your order from Roc Cloudy')
-		mail(to: 'mattwerth@mattwerth.com', subject: 'your order from Roc Cloudy')
+		mail(to: 'mattwerth@mattwerth.com; maker-box@hotmail.com', subject: 'your order from Roc Cloudy')
 	end
 end
