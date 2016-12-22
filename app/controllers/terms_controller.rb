55a3b757e901ac71c930ae3574d9ecf0431de959
matt werth <matt werth>
2016-12-23 10:21:08 +1100

    testing with terms controller

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -7,27 +7,10 @@ class TermsController < ApplicationController
     # activecustomers = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
     # contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
     discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
-    products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+    @productscreen = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
     reps = dbh.execute("SELECT * FROM sales_reps_extn").fetch(:all, :Struct)
-    @productscreen = []
-    products.each do |p|
-    	if p.Inactive == 0
-        	dbhstring = "SELECT * FROM produdefdata WHERE Code='#{p.Code}' "
-        	@saledate = dbh.execute(dbhstring).fetch(:all, :Struct)
-
-        	# if @saledate
-         #  		@saledate = @saledate.DateFld
-        	# else
-         #  		@saledate = nil
-        	# end
-        	@productscreen << @saledate
-        	@product = Product.find_by(code: p.Code)
-        	if @product
-        		@productscreen << @product.code
-        	end
-    	end
-	end
+    
 
   end
 
