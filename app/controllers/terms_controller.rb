73e884e3d28f2772ef576a1784777cbef0335b3b
matt werth <matt werth>
2016-12-23 11:56:58 +1100

    testing with terms controller

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,13 +1,18 @@
 class TermsController < ApplicationController
 		skip_before_action :authenticate_user!
   def index
+  	dbh.disconnect
+
+ActiveRecord::Base.connection.execute("BEGIN TRANSACTION; END;")
   	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
     # customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
     # activecustomers = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
     # contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
     @productscreen = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-  
+  dbh.disconnect
+
+ActiveRecord::Base.connection.execute("BEGIN TRANSACTION; END;")
 
   end
 
