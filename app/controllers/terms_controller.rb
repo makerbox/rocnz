21da9fe55597c9671e6733df8f62d0e6a3921491
matt werth <matt werth>
2017-01-04 13:09:17 +1100

    going for it with seed

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,16 +1,6 @@
 class TermsController < ApplicationController
 		skip_before_action :authenticate_user!
   def index
-  	@reply = 'starting discount list...'
-  	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-    # customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-    # activecustomers = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
-    # contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
-	@discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
-    @discounts.each do |p|
-    	@reply = @reply + p.to_s
-    end
-  dbh.disconnect
 
   end
 
