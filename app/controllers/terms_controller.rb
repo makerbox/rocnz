6c82bfb6904c264bc71c0e15b409a2782cafa332
makerbox <mattwerth@mattwerth.com>
2017-01-16 10:57:17 +1100

    from server

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,19 +3,19 @@ class TermsController < ApplicationController
   def index
 
   	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-  	discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
+  	# discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
     products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-    productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
-    @dates = []
+    # productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
+    # @dates = []
 	products.each do |p|
-		dbhstring = "SELECT * FROM produdefdata WHERE Code='#{p.Code}' " #p.Code.strip
-        saledate = dbh.execute(dbhstring).fetch(:all, :Struct)
+		# dbhstring = "SELECT * FROM produdefdata WHERE Code='#{p.Code}' " #p.Code.strip
+        # saledate = dbh.execute(dbhstring).fetch(:all, :Struct)
         # if saledate
         #   saledate = saledate.DateFld
         # else
         #   saledate = Date.today - 35.days
         # end
-        @dates << saledate
+        # @dates << saledate
     end
 
 
