4264eb752c1d9186ca6a02c01f617dafb163d47f
matt werth <matt werth>
2017-01-17 20:07:32 +1100

    I think the crashes are to do with too many requests

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -10,7 +10,10 @@ class TermsController < ApplicationController
   	  	@products.each do |p|
   	  		# @alldates.each do |d|
   	  			
-  	  			@results = dbh.preprocess_query("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct)
+  	  			if dbh.preprocess_query("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct)
+  	  				@results = 'yes'
+  	  			end
+  	  			
   	  			# 	saledate = dbh.execute("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct)
   	  			# 	@results << saledate
   	  			# 	@results << p.Code
