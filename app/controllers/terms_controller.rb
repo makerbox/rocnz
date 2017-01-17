1cca91c0afcf0f3bf174ba22f8fed2959df07bdf
matt werth <matt werth>
2017-01-17 20:59:52 +1100

    I think the crashes are to do with too many requests

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,27 +2,28 @@ class TermsController < ApplicationController
 		skip_before_action :authenticate_user!
   def index
   		@results = []
-  		RDBI.clear_error
-  		dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-  		# # @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-  		# # productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
-  	 # #    @alldates = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
+  		dbh.last_statement
+  		# dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
-  	 # #  	@products.each do |p|
-  	 # #  		if dbh.execute("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct).has_data?
-  	 # #  			@results << dbh.execute("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct)
-  	 # #  		end
+  		# # # @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+  		# # # productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
+  	 # # #    @alldates = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
 
-	   # #  #     # product = Product.where(code: p.Code.to_s.strip).first
+  	 # # #  	@products.each do |p|
+  	 # # #  		if dbh.execute("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct).has_data?
+  	 # # #  			@results << dbh.execute("SELECT DateFld FROM produdefdata WHERE Code = '#{p.Code}'").fetch(:all, :Struct)
+  	 # # #  		end
 
-	   # #  #     category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
-	   # #  #     if category != nil
-	   # #  #       category = category.strip
-	   # #  #     end
-	   # #  #     @categories << category
-	   # #  end
-	   # @results << dbh.database_name
-	    @results = RDBI.connected?
+	   # # #  #     # product = Product.where(code: p.Code.to_s.strip).first
+
+	   # # #  #     category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
+	   # # #  #     if category != nil
+	   # # #  #       category = category.strip
+	   # # #  #     end
+	   # # #  #     @categories << category
+	   # # #  end
+	   # # @results << dbh.database_name
+	   #  @results = RDBI.connected?
   end
 
 end
