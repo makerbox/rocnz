af1a5ff8425dc9aeb4b7e3f750488dd74da23f96
matt werth <matt werth>
2017-01-17 20:14:36 +1100

    I think the crashes are to do with too many requests

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,27 +3,27 @@ class TermsController < ApplicationController
   def index
   		@results = []
   		dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-  		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-  		productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
-  	    @alldates = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
+  		# @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+  		# productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
+  	 #    @alldates = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
 
-  	  	@products.each do |p|
-  	  		@alldates.each do |d|
-  	  			if d.Code == p.Code
-  	  				saledate = d.DateFld
-  	  				@results << saledate
-  	  				@results << p.Code
-  	  			end
-  	  		end
+  	 #  	@products.each do |p|
+  	 #  		@alldates.each do |d|
+  	 #  			if d.Code == p.Code
+  	 #  				saledate = d.DateFld
+  	 #  				@results << saledate
+  	 #  				@results << p.Code
+  	 #  			end
+  	 #  		end
 
-	    #     # product = Product.where(code: p.Code.to_s.strip).first
+	   #  #     # product = Product.where(code: p.Code.to_s.strip).first
 
-	    #     category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
-	    #     if category != nil
-	    #       category = category.strip
-	    #     end
-	    #     @categories << category
-	    end
+	   #  #     category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
+	   #  #     if category != nil
+	   #  #       category = category.strip
+	   #  #     end
+	   #  #     @categories << category
+	   #  end
   end
 
 end
