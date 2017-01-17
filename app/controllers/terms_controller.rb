1723abddfb3115acb7ae886dd775d5b9bb1611f0
matt werth <matt werth>
2017-01-17 14:10:45 +1100

    debugging with terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -7,13 +7,13 @@ class TermsController < ApplicationController
   		productsext = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
   	    alldates = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
   	    products.each do |p|
-	        if alldates.where(Code: p.Code)
-	          saledate = alldates.where(Code: p.Code)
-	        else
-	          saledate = Date.today - 40.days
-	        end
+	        # if alldates.where(Code: p.Code)
+	        #   saledate = alldates.where(Code: p.Code)
+	        # else
+	        #   saledate = Date.today - 40.days
+	        # end
 
-	        product = Product.where(code: p.Code.to_s.strip).first
+	        # product = Product.where(code: p.Code.to_s.strip).first
 
 	        category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
 	        if category != nil
