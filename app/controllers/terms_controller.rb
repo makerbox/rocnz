4d0f7cc4ec2ac50bd22e5b052882bd216a6af2eb
matt werth <matt werth>
2017-01-19 09:45:57 +1100

    wtf

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -39,7 +39,7 @@ class TermsController < ApplicationController
           end
         else
           newproduct = Product.new(group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-          filename = "Z:\\Attache\\Roc\\Images\\Product\\" + p.Code.to_s.strip + '.jpg'
+          filename = "Z:/Attache/Roc/Images/Product/" + p.Code.to_s.strip + '.jpg'
           # if File.exist?(filename)
             # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
             newproduct.save
