dbace420a804a6fae9cd1fa343bce17647445bbc
matt werth <matt werth>
2017-01-18 16:32:33 +1100

    gonna have to do a little loop - populate products first, then loop additional data

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,34 +1,33 @@
 class TermsController < ApplicationController
-		skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
   def index
-		@results = []
+    @results = []
 
-		dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
-		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
 
     Product.destroy_all
 
     @products.each do |p|
       code = p.Code.strip
-      description = p.Description
-      @results << code
-      @results << description.to_s.strip
-      # price1 = p.SalesPrice1
-      # price2 = p.SalesPrice2
-      # price3 = p.SalesPrice3
-      # price4 = p.SalesPrice4
-      # price5 = p.SalesPrice5
-      # rrp = p.SalesPrice6
-      # qty = p.QtyInStock
+      description = p.Description.to_s.strip
+      price1 = p.SalesPrice1
+      price2 = p.SalesPrice2
+      price3 = p.SalesPrice3
+      price4 = p.SalesPrice4
+      price5 = p.SalesPrice5
+      rrp = p.SalesPrice6
+      qty = p.QtyInStock
+      group = p.ProductGroup.to_s.strip
       # # needs category and image
-      # Product.create(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+      Product.create(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
     end
 
-    # @datedata.each do |d|
-    #   @results << Product.find(code: d.Code)
-    # end
+    @datedata.each do |d|
+      @results << Product.find(code: d.Code)
+    end
 
     dbh.disconnect
   end
