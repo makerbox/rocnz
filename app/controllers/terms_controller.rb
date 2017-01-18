c57a10daa0ca0febdfeaf4242780630385e7657c
matt werth <matt werth>
2017-01-18 22:51:09 +1100

    trying backticks to read Z:

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -26,7 +26,9 @@ class TermsController < ApplicationController
         if !Product.where(code: code).blank?
           Product.where(code: code).first.update(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
           filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + '.jpg'
-          @results << filename
+          # @results << filename
+          @results << `echo ls Z:\\Attache\\Roc\\Images\\Product\\`
+          @results << $?
           if File.exist?(filename)
             # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
             @results << 'file exists'
