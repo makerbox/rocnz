121673116aeda35e051d3ebbe9913f8198425e40
matt werth <matt werth>
2017-01-18 18:46:15 +1100

    terms almost has the whole seed in it

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -9,28 +9,32 @@ class TermsController < ApplicationController
 
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
-
+    Product.destroy_all
+    Discount.destroy_all
     @products.each do |p|
-      code = p.Code.strip
-      description = p.Description.to_s.strip
-      price1 = p.SalesPrice1
-      price2 = p.SalesPrice2
-      price3 = p.SalesPrice3
-      price4 = p.SalesPrice4
-      price5 = p.SalesPrice5
-      rrp = p.SalesPrice6
-      qty = p.QtyInStock
-      group = p.ProductGroup.to_s.strip
-      # # needs category and image
-      if Product.where(code: code)
-      else
-        Product.create(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+      if p.Inactive == 0
+        code = p.Code.strip
+        description = p.Description.to_s.strip
+        price1 = p.SalesPrice1
+        price2 = p.SalesPrice2
+        price3 = p.SalesPrice3
+        price4 = p.SalesPrice4
+        price5 = p.SalesPrice5
+        rrp = p.SalesPrice6
+        qty = p.QtyInStock
+        group = p.ProductGroup.to_s.strip
+        # # needs category and image
+        if Product.where(code: code)
+          Product.update(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+        else
+          Product.create(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+        end
       end
     end
 
     @datedata.each do |d|
       code = d.Code.strip
-      @results << Product.where(code: code).first.code
+      Product.where(code: code).update(newdate: d.DateFld)
     end
 
     @time = Time.now - @time
