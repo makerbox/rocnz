40cdb635a58ec4782d3dc0b9e77e56bf2cf15c82
matt werth <matt werth>
2017-01-18 21:13:00 +1100

    terms almost has the whole seed in it

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -23,15 +23,15 @@ class TermsController < ApplicationController
         qty = p.QtyInStock
         group = p.ProductGroup.to_s.strip
         # # needs category
-        if !Product.where(code: code).blank?
-          Product.where(code: code).first.update(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-          filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + '.jpg'
-          if File.exist?(filename)
-            Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-          else
-            Product.where(code: code).first.destroy
-          end
-        else
+        # if !Product.where(code: code).blank?
+        #   Product.where(code: code).first.update(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+        #   filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + '.jpg'
+        #   if File.exist?(filename)
+        #     Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+        #   else
+        #     Product.where(code: code).first.destroy
+        #   end
+        # else
           Product.create(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
           filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + '.jpg'
           if File.exist?(filename)
@@ -39,7 +39,7 @@ class TermsController < ApplicationController
           else
             Product.where(code: code).first.destroy
           end
-        end
+        # end
       end
     end
 
