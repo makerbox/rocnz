996dafdf2830dbcf10fbef1fc0e0b8ab50bbea68
matt werth <matt werth>
2017-01-18 22:36:22 +1100

    terms almost has the whole seed in it

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -25,7 +25,8 @@ class TermsController < ApplicationController
         # # needs category
         if !Product.where(code: code).blank?
           Product.where(code: code).first.update(code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-          filename = "Z:\\Attache\\Roc\\Images\\Product\\" + p.Code.to_s.strip + '.jpg'
+          filename = Dir["Z:\\Attache\\Roc\\Images\\Product\\*" + code + '*.jpg']
+          @results << filename
           if File.exist?(filename)
             # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
             @results << 'file exists'
