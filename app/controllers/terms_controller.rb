10b405978e4df12287457981c5930b80eec2d4e3
matt werth <matt werth>
2017-01-19 11:38:04 +1100

    finding directory

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -7,8 +7,9 @@ class TermsController < ApplicationController
     
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 
+
+# -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-    @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
 
     @products.each do |p|
       if p.Inactive == 0
@@ -26,7 +27,6 @@ class TermsController < ApplicationController
         if !Product.where(code: code).blank?
           Product.where(code: code).first.update_attributes(group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
           filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-          @results << `if exist #{filename} echo yas`
           if File.exist?(filename)
             Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
           end
@@ -41,13 +41,18 @@ class TermsController < ApplicationController
       end
     end
 
+# ------------------------GET DATES AND UPDATE THE PRODUCTS WITH new_date FIELD-----------------------
+    @datedata = dbh.execute("SELECT * FROM produdefdata").fetch(:all, :Struct)
     @datedata.each do |d|
       code = d.Code.strip
+      product = Product.where(code: code)
       @results << code
-      @results << Product.where(code: code)
+      @results << product.code
       # Product.where(code: code).first.update_attributes(new_date: d.DateFld)
     end
 
+
+# ------------------------META DATA--------------------------------------------------------------
     @results << Product.count
     @time = (Time.now - @time) / 60
     dbh.disconnect
