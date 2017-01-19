4fc466bf3d460ce281b7a6c8c605e5c7c3e4becc
matt werth <matt werth>
2017-01-19 11:06:13 +1100

    finding directory

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -27,8 +27,9 @@ class TermsController < ApplicationController
           Product.where(code: code).first.update(group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
           filename = "Z:\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
           @results << `if exist #{filename} echo yas`
+          Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
           if File.exist?(filename)
-            Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+            # upload - else delete product
           end
         else
           newproduct = Product.new(group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
