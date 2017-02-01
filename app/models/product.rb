6d4fa9941763f05c655fc771d1a86441f5f4c5ae
matt werth <matt werth>
2017-02-01 14:53:09 +1100

    discounts

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,37 +2,37 @@ class Product < ActiveRecord::Base
 has_many :quantities
 has_many :orders, through: :quantities
 
-def discount(user)
+def discount(user, price)
 	if Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
 		if !Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount
-			self.price - (self.price / 100 * discount)
+			price - (price / 100 * discount)
 		elsif !Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount).discount.nil?
 			discount = Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount).discount
-			self.price - (self.price / 100 * discount)
+			price - (price / 100 * discount)
 		elsif !Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code).discount
-			self.price - (self.price / 100 * discount)
+			price - (price / 100 * discount)
 		elsif !Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount).discount.nil?
 			discount = Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount).discount
-			self.price - (self.price / 100 * discount)
+			price - (price / 100 * discount)
 		end
 	elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
 		if !Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount
-			self.price - discount
+			price - discount
 		elsif !Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount).discount.nil?
 			discount = Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount).discount
-			self.price - discount
+			price - discount
 		elsif !Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code).discount
-			self.price - discount
+			price - discount
 		elsif !Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount.nil?
 			discount = Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount
-			self.price - discount
+			price - discount
 		end
 	else
-		self.price
+		price
 	end
 end
 
