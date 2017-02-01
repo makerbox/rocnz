92e4c20cb95a7bf0ed13214c286fa6f930c2113b
matt werth <matt werth>
2017-02-01 17:05:33 +1100

    debugging the seed now discounts work

--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -59,46 +59,46 @@ Contact.create(code:'running', email:'running')
 
 
 # ------------------------DISCOUNTS---------------------------------------------------------
-    Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
+    # Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
 
-    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-    discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
+    # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+    # discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
 
-    discounts.each do |d|
+    # discounts.each do |d|
 
-      if d.PriceCode1 == 9 #if the discount is a fixed price
-        discount = d.Price1 + d.Price2 + d.Price3 + d.Price4
-        if d.CustomerType == 10
-          customertype = 'code_fixed'
-        else
-          customertype = 'group_fixed'
-        end
-        if d.ProductType == 10
-          producttype = 'code_fixed'
-        else
-          producttype = 'group_fixed'
-        end
-      else
-        discount = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
-        if d.CustomerType == 10
-          customertype = 'code_percent'
-        else
-          customertype = 'group_percent'
-        end
-        if d.ProductType == 10
-          producttype = 'code_percent'
-        else
-          producttype = 'group_percent'
-        end
-      end
+    #   if d.PriceCode1 == 9 #if the discount is a fixed price
+    #     discount = d.Price1 + d.Price2 + d.Price3 + d.Price4
+    #     if d.CustomerType == 10
+    #       customertype = 'code_fixed'
+    #     else
+    #       customertype = 'group_fixed'
+    #     end
+    #     if d.ProductType == 10
+    #       producttype = 'code_fixed'
+    #     else
+    #       producttype = 'group_fixed'
+    #     end
+    #   else
+    #     discount = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
+    #     if d.CustomerType == 10
+    #       customertype = 'code_percent'
+    #     else
+    #       customertype = 'group_percent'
+    #     end
+    #     if d.ProductType == 10
+    #       producttype = 'code_percent'
+    #     else
+    #       producttype = 'group_percent'
+    #     end
+    #   end
 
-      if !d.Customer.blank? && !d.Product.blank?
-        Discount.create(customertype: customertype, producttype: producttype, customer: d.Customer.strip, product: d.Product.strip, discount: discount)
-      end
+    #   if !d.Customer.blank? && !d.Product.blank?
+    #     Discount.create(customertype: customertype, producttype: producttype, customer: d.Customer.strip, product: d.Product.strip, discount: discount)
+    #   end
 
-    end
+    # end
 
-    dbh.disconnect   
+    # dbh.disconnect   
 
 # ------------------------META DATA--------------------------------------------------------------
     @results << Product.count
