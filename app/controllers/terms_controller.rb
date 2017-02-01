6148d4523a1471bfea14909b8cfb27e83944c8ef
matt werth <matt werth>
2017-02-02 08:39:09 +1100

    testing discounts

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,9 +3,9 @@ class TermsController < ApplicationController
   def index
     @results = []
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-
-    Product.all.each do |p|
-      category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
+    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+    @products.each do |p|
+      category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code = '#{p.Code}' ").fetch(:all, :Struct)[0].to_h[:CostCentre]
       @results << category
     end
     dbh.disconnect
