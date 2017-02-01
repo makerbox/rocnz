f6eb8789140f540e70df675f4545ba147da7c0da
matt werth <matt werth>
2017-02-01 13:22:12 +1100

    discounts

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,109 +2,43 @@ class TermsController < ApplicationController
     skip_before_action :authenticate_user!
   def index
     @results = []
-    Discount.destroy_all #wipe the database for a clean start
+
+    Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
 
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
 
     discounts.each do |d|
-      if (d.PriceCode1 + d.PriceCode2 + d.PriceCode3 + d.PriceCode4 + d.PriceCode5 + d.PriceCode6) < 13
-        # @results << 'CUSTOMER'
-        # @results << d.Customer
-        # @results << 'PRODUCT'
-        # @results << d.Product
-        # @results << 'Customer Type'
-        # @results << d.CustomerType
-        # @results << 'Product Type'
-        # @results << d.ProductType
-        # @results << 'pricecode1'
-        # @results << d.PriceCode1
-        # @results << 'PriceAdd1'
-        # @results << d.PriceAdd1
-        # @results << 'PriceReduce1'
-        # @results << d.PriceReduce1
-        # @results << 'Disccode1'
-        # @results << d.DiscCode1
-        # @results << 'DiscPerc1'
-        # @results << d.DiscPerc1
-        # @results << 'price1--'
-        # @results << d.Price1
-        # @results << 'pricecode2'
-        # @results << d.PriceCode2
-        # @results << 'PriceAdd2'
-        # @results << d.PriceAdd2
-        # @results << 'PriceReduce2'
-        # @results << d.PriceReduce2
-        # @results << 'Disccode2'
-        # @results << d.DiscCode2
-        # @results << 'DiscPerc2'
-        # @results << d.DiscPerc2
-        # @results << 'price2--'
-        # @results << d.Price2
-        # @results << 'pricecode3'
-        # @results << d.PriceCode3
-        # @results << 'PriceAdd3'
-        # @results << d.PriceAdd3
-        # @results << 'PriceReduce3'
-        # @results << d.PriceReduce3
-        # @results << 'Disccode3'
-        # @results << d.DiscCode3
-        # @results << 'DiscPerc3'
-        # @results << d.DiscPerc3
-        # @results << 'price3--'
-        # @results << d.Price3
-        # @results << 'pricecode4'
-        # @results << d.PriceCode4
-        # @results << 'PriceAdd4'
-        # @results << d.PriceAdd4
-        # @results << 'PriceReduce4'
-        # @results << d.PriceReduce4
-        # @results << 'Disccode4'
-        # @results << d.DiscCode4
-        # @results << 'DiscPerc4'
-        # @results << d.DiscPerc4
-        # @results << 'price4--'
-        # @results << d.Price4
-      end
 
 
       if d.PriceCode1 == 9 #if the discount is a fixed price
-        percent = 0 #temporary while under construction
-        #percent = d.Price1 and flag as fixed price somehow -- still under construction
+        discount = d.Price1 + d.Price2 + d.Price3 + d.Price4
+        if d.CustomerType == 10
+          customertype = 'code_fixed'
+        else
+          customertype = 'group_fixed'
+        end
+        if d.ProductType == 10
+          producttype = 'code_fixed'
+        else
+          producttype = 'group_fixed'
+        end
       else
-        percent = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
-      end
-      if percent > 0 # check there is an actual discount to apply
-        if d.CustomerType == 10 # affect discounts for customer codes
-          if d.ProductType == 10 # affect discounts for product codes
-            if Discount.where(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'code', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          elsif d.ProductType == 30 # affect discounts for product groups
-            if Discount.where(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'code', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          end
-        elsif d.CustomerType == 30 # affect discounts for customer groups
-          if d.ProductType == 10 # affect discounts for product codes
-            if Discount.where(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'group', producttype: 'code', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          elsif d.ProductType == 30 # affect discounts for product groups
-            if Discount.where(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent) # does it exist already?
-              # do nothing
-            else
-              Discount.create(customertype: 'group', producttype: 'group', customer: d.Customer.strip, product: d.Product.strip, discount: percent)
-            end
-          end
+        discount = d.DiscPerc1 + d.DiscPerc2 + d.DiscPerc3 + d.DiscPerc4
+        if d.CustomerType == 10
+          customertype = 'code_percent'
+        else
+          customertype = 'group_percent'
+        end
+        if d.ProductType == 10
+          producttype = 'code_percent'
+        else
+          producttype = 'group_percent'
         end
       end
+
+      Product.create(customertype: customertype, producttype: producttype, customer: d.Customer.strip, product: d.Product.strip, discount: discount)
+
     end
 
     dbh.disconnect
