e92cf2255c0db3bbcde1a51c2706ece28a6a29b5
matt werth <matt werth>
2017-02-03 00:27:35 +1100

    trying to fix categories - using terms

--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -52,9 +52,9 @@ dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
     @categories.each do |cat|
-      if cat.CostCentre
+      if cat.CostCentre #if the prodmastext record has a category, then let's do it
         categorycode = cat.Code.strip
-        if Product.find_by(code: categorycode)
+        if Product.find_by(code: categorycode) #if the product exists, let's give it the category (some products without images have no dice)
           Product.find_by(code: categorycode).update_attributes(category: categorycode)
         end
       end
