45f06c441e5ab0d633e8ba964ab5c83908f9e096
matt werth <matt werth>
2017-02-02 11:38:50 +1100

    trying to fix categories - using terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -6,7 +6,7 @@ class TermsController < ApplicationController
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
     @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
     @products.each do |p|
-      category = @categories.where(code: p.Code).CostCentre.to_i.strip
+      category = @categories.to_h[:CostCentre].where(code: p.Code).CostCentre.to_i.strip
       Product.where(code: p.Code).first.update_attributes(category: category)
     end
     dbh.disconnect
