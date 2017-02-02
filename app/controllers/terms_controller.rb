6368acb9b32eb4b1e454cd99d129fec426d07a85
matt werth <matt werth>
2017-02-03 00:08:05 +1100

    trying to fix categories - using terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -4,9 +4,10 @@ class TermsController < ApplicationController
     @results = []
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-    @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct).to_h
+    @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
     @categories.each do |cat|
       @results << cat.CostCentre
+      @results << Product.find_by(code: cat.Code).code
     end
     dbh.disconnect
   end #end def index
