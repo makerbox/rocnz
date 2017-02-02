98f5bff224887ec0493d9ade205769f1a2f6d37d
matt werth <matt werth>
2017-02-03 00:29:10 +1100

    trying to fix categories - using terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,7 +2,9 @@ class TermsController < ApplicationController
     skip_before_action :authenticate_user!
   def index
     @results = []
-    
+    Product.all.each do |p|
+      @results << p.category
+    end
   end #end def index
 
 end #end class
