936b66cf8345c1a95e787e7e0760d7cdcd93835c
matt werth <matt werth>
2017-02-02 11:36:51 +1100

    trying to fix categories - using terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -4,9 +4,10 @@ class TermsController < ApplicationController
     @results = []
     dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
     @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+    @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
     @products.each do |p|
-      category = dbh.execute("SELECT CostCentre FROM prodmastext WHERE Code LIKE '#{p.Code}%' ") #[0].to_h[:CostCentre]
-      Product.where(code: p.Code).update_attributes(category: category)
+      category = @categories.where(code: p.Code).CostCentre.to_i.strip
+      Product.where(code: p.Code).first.update_attributes(category: category)
     end
     dbh.disconnect
   end #end def index
