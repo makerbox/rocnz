6685874902a6be14455bf856bfb1169af5387496
matt werth <matt werth>
2017-02-07 15:30:54 +1100

    diagnosing discounts with index

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,35 +3,35 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def discount(user, price)
-	# if Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
-		if Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
-			discount = Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount
+	# if Discount.where(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
+		if Discount.where(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
+			discount = Discount.where(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount
 			discount = (price / 100) * discount
 			price - discount
-		elsif Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount)
-			discount = Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount).discount
+		elsif Discount.where(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount)
+			discount = Discount.where(producttype: 'group_percent', product: self.group, customertype: 'group_percent', customer: user.account.discount).discount
 			discount = (price / 100) * discount
 			price - discount
-		elsif Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code)
-			discount = Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code).discount
+		elsif Discount.where(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code)
+			discount = Discount.where(producttype: 'code_percent', product: self.code, customertype: 'code_percent', customer: user.account.code).discount
 			discount = (price / 100) * discount
 			price - discount
-		elsif Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount)
-			discount = Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount).discount
+		elsif Discount.where(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount)
+			discount = Discount.where(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount).discount
 			discount = (price / 100) * discount
 			price - discount
-	# elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
-		elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
-			discount = Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount
+	# elsif Discount.where(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
+		elsif Discount.where(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
+			discount = Discount.where(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount
 			price - discount
-		elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount)
-			discount = Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount).discount
+		elsif Discount.where(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount)
+			discount = Discount.where(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount).discount
 			price - discount
-		elsif Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code)
-			discount = Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code).discount
+		elsif Discount.where(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code)
+			discount = Discount.where(producttype: 'code_fixed', product: self.code, customertype: 'code_fixed', customer: user.account.code).discount
 			price - discount
-		elsif Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount)
-			discount = Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount
+		elsif Discount.where(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount)
+			discount = Discount.where(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount
 			price - discount
 		# end
 		else
