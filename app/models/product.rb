62efe53b356b62dc63df9b6061b75bab40771619
matt werth <matt werth>
2017-02-07 15:24:10 +1100

    diagnosing discounts with index

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def discount(user, price)
-	if Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
+	# if Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code)
 		if !Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code).discount
 			discount = (price / 100) * discount
@@ -20,9 +20,8 @@ def discount(user, price)
 			discount = Discount.find_by(producttype: 'code_percent', product: self.code, customertype: 'group_percent', customer: user.account.discount).discount
 			discount = (price / 100) * discount
 			price - discount
-		end
-	elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
-		if !Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount.nil?
+	# elsif Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code)
+		elsif !Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount.nil?
 			discount = Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'code_fixed', customer: user.account.code).discount
 			price - discount
 		elsif !Discount.find_by(producttype: 'group_fixed', product: self.group, customertype: 'group_fixed', customer: user.account.discount).discount.nil?
@@ -34,10 +33,10 @@ def discount(user, price)
 		elsif !Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount.nil?
 			discount = Discount.find_by(producttype: 'code_fixed', product: self.code, customertype: 'group_fixed', customer: user.account.discount).discount
 			price - discount
+		# end
+		else
+			price
 		end
-	else
-		price
-	end
 end
 
 end #end of class
