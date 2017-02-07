775a6dd910aea6f07dd6ba80873e4407e55ba4ee
matt werth <matt werth>
2017-02-07 19:47:04 +1100

    it's just not finding the fucking discount

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,29 +8,29 @@ def calc_discount(user, price, prod_group, prod_code)
 			discount = Discount.all.find_by(producttype: 'group_percent', product: prod_group, customertype: 'code_percent', customer: user.account.code).discount
 			# discount = (price / 100) * discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount).exists?
+		elsif Discount.all.find_by(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount)
 			discount = Discount.all.find_by(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount).discount
 			discount = (price / 100) * discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code).exists?
+		elsif Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code)
 			discount = Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code).discount
 			discount = (price / 100) * discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount).exists?
+		elsif Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount)
 			discount = Discount.all.find_by(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount).discount
 			discount = (price / 100) * discount
 			price - discount
 	# elsif Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code)
-		elsif Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code).exists?
+		elsif Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code)
 			discount = Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code).discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount).exists?
+		elsif Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount)
 			discount = Discount.all.find_by(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount).discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code).exists?
+		elsif Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code)
 			discount = Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code).discount
 			price - discount
-		elsif Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount).exists?
+		elsif Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount)
 			discount = Discount.all.find_by(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount).discount
 			price - discount
 		# end
