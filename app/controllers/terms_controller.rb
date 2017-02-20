e331459e5bca1a499ecc035a364f99f5e7a28863
matt werth <matt werth>
2017-02-20 14:48:57 +1100

    discounts test in terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,13 +1,7 @@
 class TermsController < ApplicationController
     skip_before_action :authenticate_user!
   def index
-    @results = []
-    Product.all.each do |p|
-      @results << p.category
-    end
-    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-    @xray = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
-    dbh.disconnect
+	@results = Discount.all
     # system "heroku db:push"
   end #end def index
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217221137) do
+ActiveRecord::Schema.define(version: 20170220030558) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20170217221137) do
     t.integer  "discount"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "maxqty"
+    t.integer  "level"
   end
 
   create_table "mimics", force: :cascade do |t|
