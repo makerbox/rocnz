b2a9d7cc8492fa69075ac6d0bbc2813119bc1a27
matt werth <matt werth>
2017-02-23 15:26:19 +1100

    testing new discount seed method with disco function in terms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,7 +19,7 @@ end
     system "git stash"
     system "git pull"
     system "rake db:migrate"
-    @output = `git status`
+    @output = `psql --version`
       # system "rake jobs:work"
       # system "rake db:seed"
   end
