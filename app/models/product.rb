cb1989cb94c2d680ccffa5908b6f84d2793e82ba
matt werth <matt werth>
2017-02-25 12:36:20 +1100

    testing different discount processor

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ has_many :orders, through: :quantities
 
 def calc_discount(user, price, prod_group, prod_code, price_cat)
 	if Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.discount || user.account.code.strip))
-		Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.discount || user.account.code.strip))[0].discount
+		# Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.discount || user.account.code.strip))[0].discount
+		price + 50
 	else
 		price
 	end
