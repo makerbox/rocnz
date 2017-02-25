234239df69a0f84baba05ca1e2dd7daeb424f50e
matt werth <matt werth>
2017-02-25 12:24:23 +1100

    testing different discount processor

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,41 +2,43 @@ class Product < ActiveRecord::Base
 has_many :quantities
 has_many :orders, through: :quantities
 
-def calc_discount(user, price, prod_group, prod_code, prod_cat)
-	# if Discount.where(producttype: 'group_percent', product: self.group, customertype: 'code_percent', customer: user.account.code.strip)
-		if Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'code_percent', customer: user.account.code.strip).exists?
-			discount = Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'code_percent', customer: user.account.code.strip)[0].discount
-			discount = (price / 100) * discount
-			price - discount
-		elsif Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount).exists?
-			discount = Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount)[0].discount
-			discount = (price / 100) * discount
-			price - discount
-		elsif Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code.strip).exists?
-			discount = Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code.strip)[0].discount
-			discount = (price / 100) * discount
-			price - discount
-		elsif Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount).exists?
-			discount = Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount)[0].discount
-			discount = (price / 100) * discount
-			price - discount
-	# elsif Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code.strip)
-		elsif Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code.strip).exists?
-			discount = Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code.strip)[0].discount
-			price - discount
-		elsif Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount).exists?
-			discount = Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount)[0].discount
-			price - discount
-		elsif Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code.strip).exists?
-			discount = Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code.strip)[0].discount
-			price - discount
-		elsif Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount).exists?
-			discount = Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount)[0].discount
-			price - discount
+def calc_discount(user, price, prod_group, prod_code, price_cat)
+	if discount = Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.discount || user.account.code.strip))[0].discount
+		discount
+	else
+		price
+	end
+		# if Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'code_percent', customer: user.account.code.strip).exists?
+		# 	discount = Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'code_percent', customer: user.account.code.strip)[0].discount
+		# 	discount = (price / 100) * discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount).exists?
+		# 	discount = Discount.where(producttype: 'group_percent', product: prod_group, customertype: 'group_percent', customer: user.account.discount)[0].discount
+		# 	discount = (price / 100) * discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code.strip).exists?
+		# 	discount = Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'code_percent', customer: user.account.code.strip)[0].discount
+		# 	discount = (price / 100) * discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount).exists?
+		# 	discount = Discount.where(producttype: 'code_percent', product: prod_code, customertype: 'group_percent', customer: user.account.discount)[0].discount
+		# 	discount = (price / 100) * discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code.strip).exists?
+		# 	discount = Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'code_fixed', customer: user.account.code.strip)[0].discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount).exists?
+		# 	discount = Discount.where(producttype: 'group_fixed', product: prod_group, customertype: 'group_fixed', customer: user.account.discount)[0].discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code.strip).exists?
+		# 	discount = Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'code_fixed', customer: user.account.code.strip)[0].discount
+		# 	price - discount
+		# elsif Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount).exists?
+		# 	discount = Discount.where(producttype: 'code_fixed', product: prod_code, customertype: 'group_fixed', customer: user.account.discount)[0].discount
+		# 	price - discount
+		# else
+		# 	price
 		# end
-		else
-			price
-		end
 end
 
 end #end of class
