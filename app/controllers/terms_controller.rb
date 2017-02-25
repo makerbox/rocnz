840744aa6476f4f362a3dee5e03d268e2eef5b4c
matt werth <matt werth>
2017-02-25 11:45:16 +1100

    testing new disco in terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -48,90 +48,63 @@ class TermsController < ApplicationController
         level = 1
         maxqty = d.MaxQty1
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode2 == 9) || (d.DiscCode2 == 9) 
         percentage = d.DiscPerc2
         fixed = d.Price2
         fixedprice = d.PriceCode2
         level = 2
         maxqty = d.MaxQty2
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode3 == 9) || (d.DiscCode3 == 9) 
         percentage = d.DiscPerc3
         fixed = d.Price3
         fixedprice = d.PriceCode3
         level = 3
         maxqty = d.MaxQty3
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode4 == 9) || (d.DiscCode4 == 9) 
         percentage = d.DiscPerc4
         fixed = d.Price4
         fixedprice = d.PriceCode4
         level = 4
         maxqty = d.MaxQty4
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode5 == 9) || (d.DiscCode5 == 9) 
         percentage = d.DiscPerc5
         fixed = d.Price5
         fixedprice = d.PriceCode5
         level = 5
         maxqty = d.MaxQty5
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode6 == 9) || (d.DiscCode6 == 9) 
         percentage = d.DiscPerc6
         fixed = d.Price6
         fixedprice = d.PriceCode6
         level = 6
         maxqty = d.MaxQty6
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode7 == 9) || (d.DiscCode7 == 9) 
         percentage = d.DiscPerc7
         fixed = d.Price7
         fixedprice = d.PriceCode7
         level = 7
         maxqty = d.MaxQty7
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode8 == 9) || (d.DiscCode8 == 9) 
         percentage = d.DiscPerc8
         fixed = d.Price8
         fixedprice = d.PriceCode8
         level = 8
         maxqty = d.MaxQty8
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode9 == 9) || (d.DiscCode9 == 9) 
         percentage = d.DiscPerc9
         fixed = d.Price9
         fixedprice = d.PriceCode9
         level = 9
         maxqty = d.MaxQty9
       end
-      if !d.Customer.blank? && !d.Product.blank?
-        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType)
-      end
-      if (d.PriceCode1 == 9) || (d.DiscCode1 == 9) 
+      if (d.PriceCode10 == 9) || (d.DiscCode10 == 9) 
         percentage = d.DiscPerc10
         fixed = d.Price10
         fixedprice = d.PriceCode10
