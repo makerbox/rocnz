df1b10e095e64253f016c940e15fa281bd41a84e
matt werth <matt werth>
2017-02-27 21:27:03 +1100

    testing took out some code from product index that needs to go back after test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def calc_discount(user, price, prod_group, prod_code, price_cat)
-	if Discount.where("product = (prod_group OR prod_code OR price_cat)", "customer = (user.account.discount OR user.account.code.strip)")
-		@disc = Discount.find_by("product = (prod_group OR prod_code OR price_cat)", "customer = (user.account.discount OR user.account.code.strip)")
+	if Discount.all.where(product: (prod_group OR prod_code OR price_cat), customer: (user.account.discount OR user.account.code.strip))
+		@disc = Discount.all.find_by(product: (prod_group OR prod_code OR price_cat), customer: (user.account.discount OR user.account.code.strip))
 		@disc
 	else
 		price
