539c8ccd590655a6242ca06fd97eceabafbdafcd
matt werth <matt werth>
2017-02-27 20:41:27 +1100

    testing discounts in terms - seed should be good now

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,7 +2,7 @@ class TermsController < ApplicationController
     skip_before_action :authenticate_user!
   def index
 	@results = []
-	@results = Discount.where(customer: (current_user.mimic.user.account.discount || current_user.mimic.user.account.code.strip))
+	@results = Discount.where(customer: (current_user.mimic.account.discount || current_user.mimic.account.code.strip))
 
   end #end def index
 
