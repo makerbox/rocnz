df312651fa7cb72eb11b80a1751ecc24dca0e889
matt werth <matt werth>
2017-03-01 14:05:41 +1100

    rolling back from local images - need cloudinary

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,9 +2,8 @@ class TermsController < ApplicationController
     skip_before_action :authenticate_user!
   def index
 	@results = []	
-	Product.all.each do |newd|
-		@results << newd.new_date
-		@results << newd.code
+	Discount.all.each do |disc|
+		@results << disc.product
 		@results << '......................'
 	end
 	@results << Discount.all.count
