df9e393a8c24d8cab7cc3ed5d482b8434b3c0558
matt werth <matt werth>
2017-03-07 20:52:58 +1100

    removed terms tester- discounts are good to seed

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,9 +2,6 @@ class TermsController < ApplicationController
 	skip_before_action :authenticate_user!
 	def index
 
-@results = []	
-@results = Discount.all
-
   end #end def index
 
 end #end class
