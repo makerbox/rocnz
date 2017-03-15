327fe34a76595cd7ede2c10ec70e9f9b0086335d
matt werth <matt werth>
2017-03-15 19:35:40 +1100

    debugging discounts again

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,16 +3,16 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def calc_discount(u, price, prod_group, prod_code, price_cat)
-	if Discount.where(product: (prod_group || prod_code || price_cat)).where(customer: u.account.code.strip)
+	# if Discount.where(product: (prod_group || prod_code || price_cat)).where(customer: u.account.code.strip)
 		disco = Discount.where(product: (prod_group || prod_code || price_cat)).where(customer: u.account.code.strip).first
 		if disco.disctype == 'fixedtype'
 			price - disco.discount
 		else
 			price - (price * (disco.discount / 100))
 		end
-	else
-		price
-	end
+	# else
+	# 	price
+	# end
 end
 
 def show_discount(u, prod_group, prod_code, price_cat)
