bced7215cd892241ec9bdd5b82addd10f9112f40
matt werth <matt werth>
2017-03-16 09:52:44 +1100

    discount debug

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def calc_discount(u, price, prod_group, prod_code, price_cat)
-	if Discount.all.find_by(product: prod_group, customer: u.account.code.strip)
-		disco = Discount.all.find_by(product: prod_group, customer: u.account.code.strip)
+	if Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+		disco = Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
 		if disco.disctype == 'fixedtype'
 			result = price - disco.discount
 		else
@@ -16,8 +16,6 @@ def calc_discount(u, price, prod_group, prod_code, price_cat)
 	return result
 end
 
-def show_discount(u, prod_group, prod_code, price_cat)
-	prod_group + '..' + prod_code + '..' + price_cat + '..' + u.account.code.strip
-end
+
 
 end #end of class
