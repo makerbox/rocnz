fa5824f3fc517ab15077d5a81b2d124e74c3f600
matt werth <matt werth>
2017-03-15 18:32:42 +1100

    found the problem in discount function not checking customer

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ActiveRecord::Base
 has_many :quantities
 has_many :orders, through: :quantities
 
-def calc_discount(u, price, prod_group, prod_code, price_cat)
-	if Discount.where(product: (prod_group || prod_code || price_cat)).where(customer: u.account.code).exists?
-		disco = Discount.where(product: (prod_group || prod_code || price_cat)).where(customer: u.account.code).first
+def calc_discount(user, price, prod_group, prod_code, price_cat)
+	if Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.code.strip || user.account.discount)).exists?
+		disco = Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.code.strip || user.account.discount)).first
 		if disco.disctype == 'fixedtype'
 			price - disco.discount
 		else
@@ -15,15 +15,14 @@ def calc_discount(u, price, prod_group, prod_code, price_cat)
 	end
 end
 
-# def show_discount(u, price, prod_group, prod_code, price_cat)
-def show_discount(u)
-	# if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
-	# 	disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
-	# 	result = disco.producttype + ' ' + disco.product + '-$' + disco.discount.to_s
-	# else
-	# 	result = 'no discount'
-	# end
-	u.account.code
+def show_discount(user, price, prod_group, prod_code, price_cat)
+	if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
+		disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
+		result = disco.producttype + ' ' + disco.product + '-$' + disco.discount.to_s
+	else
+		result = 'no discount'
+	end
+	result
 end
 
 end #end of class
