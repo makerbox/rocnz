5416549fab4d0d8daa589e4c8f8a1f4fec9fee92
matt werth <matt werth>
2017-03-15 18:13:25 +1100

    found the problem in discount function not checking customer

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ has_many :quantities
 has_many :orders, through: :quantities
 
 def calc_discount(user, price, prod_group, prod_code, price_cat)
-	if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
-		disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
+	if Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.code || user.account.discount)).exists?
+		disco = Discount.where(product: (prod_group || prod_code || price_cat), customer: (user.account.code || user.account.discount)).first
 		if disco.disctype == 'fixedtype'
 			puts disco.producttype
 			price - disco.discount
