65e1afb5331ee2070b06fb2e10854f6f46762e1b
matt werth <matt werth>
2017-03-16 10:52:33 +1100

    new registration email direction

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,6 +3,6 @@ class AdminMailer < ApplicationMailer
 	
 	def new_user_waiting_for_approval(emails)
 		@emails = emails
-		mail(to: @emails, subject: 'Pending Approval - RocCloudy')
+		mail(to: 'cheryl@roccloudy.com, web@roccloudy.com', subject: 'Pending Approval - RocCloudy')
 	end
 end
