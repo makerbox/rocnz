4f4210c4f691a2d16dd303c88388850e8b19b68b
matt werth <matt werth>
2017-03-15 18:29:03 +1100

    found the problem in discount function not checking customer

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,14 +15,15 @@ def calc_discount(u, price, prod_group, prod_code, price_cat)
 	end
 end
 
-def show_discount(user, price, prod_group, prod_code, price_cat)
-	if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
-		disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
-		result = u.account.code + ' ' + disco.producttype + ' ' + disco.product + '-$' + disco.discount.to_s
-	else
-		result = 'no discount'
-	end
-	result
+# def show_discount(u, price, prod_group, prod_code, price_cat)
+def show_discount(u)
+	# if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
+	# 	disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
+	# 	result = disco.producttype + ' ' + disco.product + '-$' + disco.discount.to_s
+	# else
+	# 	result = 'no discount'
+	# end
+	u.account.code
 end
 
 end #end of class
