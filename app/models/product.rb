7bee285976e1fe13e662e533d61d43b00680b171
matt werth <matt werth>
2017-03-15 11:04:36 +1100

    checking discount by messing with function

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ def calc_discount(user, price, prod_group, prod_code, price_cat)
 	if Discount.where(product: (prod_group || prod_code || price_cat)).exists?
 		disco = Discount.where(product: (prod_group || prod_code || price_cat)).first
 		if disco.disctype == 'fixedtype'
+			puts disco.producttype
 			price - disco.discount
 		else
 			price - (price * (disco.discount / 100))
