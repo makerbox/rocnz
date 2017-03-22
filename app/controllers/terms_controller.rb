ffd5570fd49a6a1207ca09d64386680d209aa034
matt werth <matt werth>
2017-03-23 10:03:38 +1100

    testing customer account seed in terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,7 +1,37 @@
 class TermsController < ApplicationController
 	skip_before_action :authenticate_user!
 	def index
-	
+	@customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+@results = []
+    @customers.each do |c|
+      code = c.Code
+      compname = c.Name
+      street = c.Street
+      suburb = c.Suburb 
+      postcode = c.Postcode 
+      phone = c.Phone 
+      sort = c.Sort 
+      discount = c.SpecialPriceCat 
+      seller_level = c.PriceCat
+
+      # code
+      # name
+      # street
+      # suburb
+      # postcode
+      # phone
+      # Contact
+      # sort
+      # territory
+      # SalesRep
+      # cat
+      # PriceCat
+      # specialpricecat
+      # (camelcase)
+      if Account.all.find_by(code: code)
+        @results << code
+      end
+    end
   end #end def index
 
 end #end class
--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -228,6 +228,45 @@ dbh.disconnect
     dbh.disconnect 
     @results = Discount.all
 
+# -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
+@customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+@results = []
+    @customers.each do |c|
+      code = c.Code
+      compname = c.Name
+      street = c.Street
+      suburb = c.Suburb 
+      postcode = c.Postcode 
+      phone = c.Phone 
+      sort = c.Sort 
+      discount = c.SpecialPriceCat 
+      seller_level = c.PriceCat
+
+      # code
+      # name
+      # street
+      # suburb
+      # postcode
+      # phone
+      # Contact
+      # sort
+      # territory
+      # SalesRep
+      # cat
+      # PriceCat
+      # specialpricecat
+      # (camelcase)
+      if Account.all.find_by(code: code)
+        @results << code
+      end
+    end
+# -------------------------GET CUSTOMER EXT INFO AND UPDATE / ADD ON TO EACH CUSTOMER----------------------
+code
+countrycode
+CostCentre
+emailaddr
+inactivecust #give accounts an active field to check this with and clear inactivecust 
+custstate
 # ------------------------META DATA--------------------------------------------------------------
 @results << Product.count
 @time = (Time.now - @time) / 60
