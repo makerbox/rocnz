732fde2d01a4cfa02a643073cd7db6ffd53c561f
matt werth <matt werth>
2017-03-23 20:36:05 +1100

    testing seed in terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -29,8 +29,7 @@ class TermsController < ApplicationController
     # PriceCat
     # specialpricecat
     # (camelcase)
-    @account = Account.new(rep: rep, sort: sort, discount: discount, seller_level: seller_level, code: code, company: compname, approved:'approved',  street: street, suburb: suburb, postcode: postcode, phone: phone)
-    @account.save
+    @account = Account.create(rep: rep, sort: sort, discount: discount, seller_level: seller_level, code: code, company: compname, approved:'approved', street: street, suburb: suburb, postcode: postcode, phone: phone)
     @results << @account.rep
   end
 dbh.disconnect
