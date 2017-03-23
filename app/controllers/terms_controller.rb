26bc308f800cf45ceb5698e2fb111caecc03f244
matt werth <matt werth>
2017-03-24 08:04:22 +1100

    testing in terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -11,40 +11,42 @@ class TermsController < ApplicationController
 				@results << code
 				@results << ce.CostCentre
 				@results << ce.CountryCode
-				# Account.create(code: code)
+					# Account.create(code: code)
+				end
 			end
 		end
+		@customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+		@customers.each do |c|
+			if account = Account.all.find_by(code: c.Code.strip)
+				compname = c.Name
+				street = c.Street
+				suburb = c.Suburb 
+				postcode = c.Postcode 
+				phone = c.Phone 
+				sort = c.Sort 
+				discount = c.SpecialPriceCat 
+				seller_level = c.PriceCat
+				rep = c.SalesRep
+				account.update_attributes(company: compname)
+			end
+		    # code
+		    # name
+		    # street
+		    # suburb
+		    # postcode
+		    # phone
+		    # Contact
+		    # sort
+		    # territory
+		    # SalesRep
+		    # cat
+		    # PriceCat
+		    # specialpricecat
+		    # (camelcase)
 
-  # @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-  # @customers.each do |c|
-  #   code = c.Code
-  #   compname = c.Name
-  #   street = c.Street
-  #   suburb = c.Suburb 
-  #   postcode = c.Postcode 
-  #   phone = c.Phone 
-  #   sort = c.Sort 
-  #   discount = c.SpecialPriceCat 
-  #   seller_level = c.PriceCat
-  #   rep = c.SalesRep
-  #   # code
-  #   # name
-  #   # street
-  #   # suburb
-  #   # postcode
-  #   # phone
-  #   # Contact
-  #   # sort
-  #   # territory
-  #   # SalesRep
-  #   # cat
-  #   # PriceCat
-  #   # specialpricecat
-  #   # (camelcase)
-
-  #   # Account.create(rep: rep)
-  # end
-dbh.disconnect
+		    # Account.create(rep: rep)
+		end
+		dbh.disconnect
   	end #end def index
 
 end #end class
