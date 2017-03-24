54bff42e9f03bac71b9729ddbf9bf142d8cbd382
matt werth <matt werth>
2017-03-24 11:35:58 +1100

    testing in terms

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp  
 config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {  
-  address: "mail.roccloudy.com",
+  address: "gator4040.hostgator.com",
   domain: "roccloudy.com",
-  port: 25,
+  port: 465,
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: "web@roccloudy.com",
