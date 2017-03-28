5c3c3a9f976ef7876cb5aaae2c8265efc2294504
matt werth <matt werth>
2017-03-28 14:39:21 +1100

    new user save

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,10 +1,6 @@
 class TermsController < ApplicationController
 	skip_before_action :authenticate_user!
-	def index
-		UserMailer.welcome_email('mattwerth@mattwerth.com').deliver_now
-		@results = []
-		# Account.destroy_all
-		
+	def index		
   	end #end def index
 
 end #end class
