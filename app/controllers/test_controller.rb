789ac3915b2bf70bd8eca0f71518072412bcf3ad
matt werth <matt werth>
2017-03-29 10:06:01 +1100

    testing contacts again in test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,7 +3,24 @@ class TestController < ApplicationController
   def index
   	@result = []
   	# -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
-  	Account.all.each do |acct|
+  	dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+  	@contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
+	@contacts.each do |contact|
+	  if contact.Active == 'Yes'
+	    email = contact.EmailAddress
+	    code = contact.Code
+	    newcontact = Contact.new(email: email, code: code)
+	    if thisaccount = Account.all.find_by(code: code)
+	      thisaccount.user.update_attributes(email: email)
+	      @result << 'found'
+		else
+		  @result << 'none'
+	    end
+	  end
+	end
+	dbh.disconnect
+
+	Account.all.each do |acct|
   		@result << acct.user.email
   	end
 
