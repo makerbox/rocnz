7d68ffd971ff60ac6ef97b8b19da89bfd9de8c97
matt werth <matt werth>
2017-03-28 16:41:51 +1100

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,7 @@
 class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
+  	@result = []
   	# -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
 dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
@@ -31,7 +32,8 @@ end
     seller_level = c.PriceCat
     rep = c.SalesRep
     account.update_attributes(phone: phone, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
-    account.user.update_attributes(email: email)
+    # account.user.update_attributes(email: email)
+    @result << account.user.email
   end
 end
 dbh.disconnect
