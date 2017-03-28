d19077d28aff1cf27d110ff5b1342a7940a948ee
matt werth <matt werth>
2017-03-28 18:35:23 +1100

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,42 +3,42 @@ class TestController < ApplicationController
   def index
   	@result = []
   	# -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
-Account.destroy_all
-User.destroy_all
-dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-iteration = 0
-@customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
-@customers_ext.each do |ce|
-  if ce.InactiveCust == 0
-    code = ce.Code.strip
-    if !Account.all.find_by(code: code)
-      iteration += 1
-      newuser = User.new(email: iteration, password: "roccloudyportal", password_confirmation: "roccloudyportal") #create the user
-      if newuser.save(:validate => false) #false to skip validation
-        newuser.add_role :user
-        newaccount = Account.new(code: code, user: newuser) #create the account and associate with user
-        newaccount.save
-      end
-    end
-  end
-end
-@customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-@customers.each do |c|
-  if Account.all.find_by(code: c.Code.strip)
-    account = Account.all.find_by(code: c.Code.strip)
-    compname = c.Name
-    street = c.Street
-    suburb = c.Suburb 
-    postcode = c.Postcode 
-    phone = c.Phone 
-    sort = c.Sort 
-    discount = c.SpecialPriceCat 
-    seller_level = c.PriceCat
-    rep = c.SalesRep
-    account.update_attributes(phone: phone, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
-    account.user.update_attributes(email: email)
-  end
-end
-dbh.disconnect
+# Account.destroy_all
+# User.destroy_all
+# dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+# iteration = 0
+# @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
+# @customers_ext.each do |ce|
+#   if ce.InactiveCust == 0
+#     code = ce.Code.strip
+#     if !Account.all.find_by(code: code)
+#       iteration += 1
+#       newuser = User.new(email: iteration, password: "roccloudyportal", password_confirmation: "roccloudyportal") #create the user
+#       if newuser.save(:validate => false) #false to skip validation
+#         newuser.add_role :user
+#         newaccount = Account.new(code: code, user: newuser) #create the account and associate with user
+#         newaccount.save
+#       end
+#     end
+#   end
+# end
+# @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+# @customers.each do |c|
+#   if Account.all.find_by(code: c.Code.strip)
+#     account = Account.all.find_by(code: c.Code.strip)
+#     compname = c.Name
+#     street = c.Street
+#     suburb = c.Suburb 
+#     postcode = c.Postcode 
+#     phone = c.Phone 
+#     sort = c.Sort 
+#     discount = c.SpecialPriceCat 
+#     seller_level = c.PriceCat
+#     rep = c.SalesRep
+#     account.update_attributes(phone: phone, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
+#     account.user.update_attributes(email: email)
+#   end
+# end
+# dbh.disconnect
   end
 end
