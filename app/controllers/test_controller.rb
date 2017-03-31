5425f6a13f5db7cf2db2367d3335ad4c63306983
matt werth <matt werth>
2017-03-31 14:16:04 +1100

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,24 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-
-dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-#-------------------------- CREATE REP ACCOUNTS -----------------------------------
-    @reps = dbh.execute("SELECT * FROM sales_reps_extn").fetch(:all, :Struct)
-      @reps.each do |rep|
-        @result << rep.Inactive
-        if rep.Inactive == 'N'
-          code = rep.Code
-          email = rep.EmailAddress
-          repuser = User.new(email: email, password: 'cloudy_rep_123', password_confirmation: 'cloudy_rep_123')
-          if repuser.save(validate: false)
-            @result << repuser.email
-            repaccount = Account.new(code: code, user: repuser)
-            repaccount.save
-            repuser.add_role :admin
-          end
-        end
-    end
+    @result << User.all
 
   end
 end
