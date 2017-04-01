d54f3c12e8562b81413761b7885fbfc921b781a9
matt werth <matt werth>
2017-04-02 09:08:23 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-    @result = User.all
 counter = 0
 dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
 @customers_ext = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
@@ -21,6 +20,7 @@ dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
           newuser.add_role :user
           newaccount = Account.new(code: code, user: newuser) #create the account and associate with user
           newaccount.save
+          @result << 'newuser save - maybe account save too'
         end
       end
     end
@@ -42,41 +42,41 @@ end
     account.update_attributes(approved: 'approved', phone: phone, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
   end
 end
-@contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
-@contacts.each do |contact|
-  if contact.Active == 1
-    email = contact.EmailAddress
-    code = contact.Code
-    newcontact = Contact.new(email: email, code: code)
-    if thisaccount = Account.all.find_by(code: code)
-      thisaccount.user.update_attributes(email: email)
-      newcontact.save
-    end
-  end
-end
-dbh.disconnect
+# @contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
+# @contacts.each do |contact|
+#   if contact.Active == 1
+#     email = contact.EmailAddress
+#     code = contact.Code
+#     newcontact = Contact.new(email: email, code: code)
+#     if thisaccount = Account.all.find_by(code: code)
+#       thisaccount.user.update_attributes(email: email)
+#       newcontact.save
+#     end
+#   end
+# end
+# dbh.disconnect
 
-#-------------------------- CREATE ADMIN USER -------------------------------------
-adminuser = User.all.find_by(email: 'web@roccloudy.com')
-adminuser.add_role :admin
-adminuser.account.update_attributes(approved: 'approved')
+# #-------------------------- CREATE ADMIN USER -------------------------------------
+# adminuser = User.all.find_by(email: 'web@roccloudy.com')
+# adminuser.add_role :admin
+# adminuser.account.update_attributes(approved: 'approved')
 
-#-------------------------- CREATE REP ACCOUNTS -----------------------------------
-dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-@reps = dbh.execute("SELECT * FROM sales_reps_extn").fetch(:all, :Struct)
-@reps.each do |rep|
-  if rep.Inactive == 'N'
-    code = rep.Code
-    if email = rep.EmailAddress
-      repuser = User.new(email: email, password: 'cloudy_rep_123', password_confirmation: 'cloudy_rep_123')
-      if repuser.save(validate: false)
-        repaccount = Account.new(code: code, user: repuser)
-        repaccount.save
-        repuser.add_role :admin
-      end
-    end
-  end
-end
-dbh.disconnect
+# #-------------------------- CREATE REP ACCOUNTS -----------------------------------
+# dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+# @reps = dbh.execute("SELECT * FROM sales_reps_extn").fetch(:all, :Struct)
+# @reps.each do |rep|
+#   if rep.Inactive == 'N'
+#     code = rep.Code
+#     if email = rep.EmailAddress
+#       repuser = User.new(email: email, password: 'cloudy_rep_123', password_confirmation: 'cloudy_rep_123')
+#       if repuser.save(validate: false)
+#         repaccount = Account.new(code: code, user: repuser)
+#         repaccount.save
+#         repuser.add_role :admin
+#       end
+#     end
+#   end
+# end
+# dbh.disconnect
   end
 end
