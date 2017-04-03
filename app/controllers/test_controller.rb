812cc5ce91c959b2cefd3b230936110c6fb732c1
matt werth <matt werth>
2017-04-03 11:23:56 +1000

    testing nz

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,7 +3,7 @@ class TestController < ApplicationController
   def index
   	@result = []
   	dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
-  	@result = dbh.tables
+  	@result = dbh.func(:proto_info)
  #    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
  #    @products.each do |p|
  #      if p.Inactive == 0
