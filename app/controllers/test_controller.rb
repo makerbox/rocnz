5d0756f2e6d2e719d76a8d7e1c17e377c338ae22
matt werth <matt werth>
2017-04-03 10:46:18 +1000

    nz test only

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,31 +2,21 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-    # User.destroy_all
-    # Account.destroy_all
-    # Contact.destroy_all
-#-------------------------- CREATE ADMIN USER -------------------------------------
-
-    @result << current_user.account
-
-
-#-------------------------- CREATE REP ACCOUNTS -----------------------------------
-# dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-# @reps = dbh.execute("SELECT * FROM sales_reps_extn").fetch(:all, :Struct)
-# @reps.each do |rep|
-#   if rep.Inactive == 'N'
-#     code = rep.Code
-#     if email = rep.EmailAddress
-#       repuser = User.new(email: email, password: 'cloudy_rep_123', password_confirmation: 'cloudy_rep_123')
-#       if repuser.save(validate: false)
-#         repaccount = Account.new(code: code, user: repuser)
-#         repaccount.save
-#         repuser.add_role :admin
-#       end
-#     end
-#   end
-# end
-# dbh.disconnect
+    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+    @products.each do |p|
+      if p.Inactive == 0
+        code = p.Code.strip
+        description = p.Description.to_s.strip
+        price1 = p.SalesPrice1
+        price2 = p.SalesPrice2
+        price3 = p.SalesPrice3
+        price4 = p.SalesPrice4
+        price5 = p.SalesPrice5
+        rrp = p.SalesPrice6
+        qty = p.QtyInStock
+        @result << code + ' - ' + price1.to_s
+	  end
+	end
 
   end
 end
