6e26e731d73f0bc61ccbf894a2ab850b8a17462e
matt werth <matt werth>
2017-04-03 10:50:48 +1000

    nz test only

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,22 +2,23 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-  	dbh = RDBI.connect :ODBC, :db => "wholesalenz"
-    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-    @products.each do |p|
-      if p.Inactive == 0
-        code = p.Code.strip
-        description = p.Description.to_s.strip
-        price1 = p.SalesPrice1
-        price2 = p.SalesPrice2
-        price3 = p.SalesPrice3
-        price4 = p.SalesPrice4
-        price5 = p.SalesPrice5
-        rrp = p.SalesPrice6
-        qty = p.QtyInStock
-        @result << code + ' - ' + price1.to_s
-	  end
-	end
+  	dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+  	@result = dbh.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES")
+ #    @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+ #    @products.each do |p|
+ #      if p.Inactive == 0
+ #        code = p.Code.strip
+ #        description = p.Description.to_s.strip
+ #        price1 = p.SalesPrice1
+ #        price2 = p.SalesPrice2
+ #        price3 = p.SalesPrice3
+ #        price4 = p.SalesPrice4
+ #        price5 = p.SalesPrice5
+ #        rrp = p.SalesPrice6
+ #        qty = p.QtyInStock
+ #        @result << code + ' - ' + price1.to_s
+	#   end
+	# end
 
   end
 end
