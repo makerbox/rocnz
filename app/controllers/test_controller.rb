cd71f0f1f802b9a4ec1b34dcd54babf3b4175064
matt werth <matt werth>
2017-04-05 08:53:11 +1000

    email works! now on to the mimic exit button

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,5 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-  	AdminMailer.new_user_waiting_for_approval('web@roccloudy.com', Account.last).deliver_now
   end
 end
