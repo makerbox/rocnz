1093b638341a48b6bd907b33af3e4f140d5a8990
matt werth <matt werth>
2017-04-04 16:05:02 +1000

    testing wtf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,9 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-  	AdminMailer.new_user_waiting_for_approval('web@roccloudy.com', Account.last).deliver_now
+  	Product.all.each do |p|
+  		@result << p.code
+  	end
+  	# AdminMailer.new_user_waiting_for_approval('web@roccloudy.com', Account.last).deliver_now
   end
 end
