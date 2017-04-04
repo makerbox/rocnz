1da1a1d91e96882a12cf3fd8a31967c3777370f7
matt werth <matt werth>
2017-04-04 11:23:45 +1000

    testing email to admin

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-  	EmailJob.perform_async('cheryl@roccloudy.com, web@roccloudy.com')
+  	EmailJob.perform_async('web@roccloudy.com')
   end
 end
