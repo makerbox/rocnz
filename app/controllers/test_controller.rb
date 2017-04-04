90a95e36f0ff3b14b6a68abd50c272eb27a9f7f2
matt werth <matt werth>
2017-04-04 14:18:02 +1000

    trying new email server

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
   	@result = []
-  	AdminMailer.new_user_waiting_for_approval('web@roccloudy.com', Account.first).deliver_now
+  	AdminMailer.new_user_waiting_for_approval('web@roccloudy.com', Account.last).deliver_now
   end
 end
