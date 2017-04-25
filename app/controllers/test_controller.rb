004f8ddddece418c96f0b814d172ec28b5ca7fb1
matt werth <matt werth>
2017-04-25 20:05:36 +1000

    push to heroku

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,6 @@
 class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
-  	@result = []
+  	@result = `heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068`
   end
 end
