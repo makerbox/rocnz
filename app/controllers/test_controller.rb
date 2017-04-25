76a932c3144526ea316c4aef52bce1619e5fa383
matt werth <matt werth>
2017-04-26 06:18:08 +1000

    clockwork - added to test controller

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,7 @@
 class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
+  	system "clockwork clock.rb"
   	system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
   end
 end
