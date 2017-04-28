bd9922bd3e2ed6641dfbb714a1940e8ccfe35208
matt werth <matt werth>
2017-04-28 19:52:21 +1000

    testing in test

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,15 +16,15 @@ class User < ActiveRecord::Base
 
   def self.checksort
     if !self.has_role? :admin #if they ain't admin - they just have normal sort
-      @sort = self.account.sort
+      output = self.account.sort
     else
       if self.mimic #if they is admin and have mimic - give them mimic sort
-        @sort = self.mimic.account.sort
+        output = self.mimic.account.sort
       else #if they admin without mimic - they get full sort
-        @sort = 'R L U P'
+        output = 'R L U P'
       end
     end
-    return @sort
+    return output
   end
 
 end
