82db0c69a76d03d3a19df3bcae70039b54c5b5db
matt werth <matt werth>
2017-04-28 15:07:21 +1000

    fixing sort bug with application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
 
+if !current_user.has_role? :admin #if they ain't admin - they just have normal sort
+	@sort = current_user.account.sort
+else
+	if current_user.mimic #if they is admin and have mimic - give them mimic sort
+		@sort = current_user.mimic.account.sort
+	else #if they admin without mimic - they get full sort
+		@sort = 'R L U P'
+	end
+end
+
+
 end
