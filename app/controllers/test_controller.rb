1414af5ece52ef9a04432a018266a5ed9127f815
matt werth <matt werth>
2017-04-28 19:49:14 +1000

    omg controller model shit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,10 @@
 class TestController < ApplicationController
 	skip_before_action :authenticate_user!
   def index
-  	system "clockwork clock.rb"
-  	system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
+  	# system "clockwork clock.rb"
+  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
+
+  	@sort = current_user.checksort
+  	
   end
 end
