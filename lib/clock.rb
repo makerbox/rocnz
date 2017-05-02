33c52bb7ec0cfa047cb23dd3cc483aea0e3be68f
matt werth <matt werth>
2017-05-02 13:27:55 +1000

    testing schedule with contact creation every ten mins

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,6 +1,10 @@
 require 'clockwork'
-require './config/boot'
-require './config/environment'
+# require './config/boot'
+# require './config/environment'
+include Clockwork
 
+handler do |job|
+  puts "Running #{job}"
+end
 
 every(30.minutes, 'populate') { Delayed::Job.enqueue PopulateJob.new}
