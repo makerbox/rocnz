1c8dffc5616597e8abdf12111bd69192ce7fc2a6
matt werth <matt werth>
2017-05-02 16:39:55 +1000

    added perform to clockwork

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,4 +8,4 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(30.minutes, 'populate') { Delayed::Job.enqueue PopulateJob.new}
+every(30.minutes, 'populate') { Delayed::Job.enqueue PopulateJob.new.perform}
