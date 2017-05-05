4fac70406454c595c1f98e36d0a183600fa4a318
matt werth <matt werth>
2017-05-05 12:28:21 +1000

    testing populate accounts in test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -42,7 +42,7 @@ class TestController < ApplicationController
 
 	  	# Contact.all.find_by(code:'running').destroy
 	  	# Contact.all.find_by(code:'clock').destroy
-	  	@output = Contact.all.where(code:'clock')
+	  	# @output = Contact.all.where(code:'clock')
 
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
 	end
