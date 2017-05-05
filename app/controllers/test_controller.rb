213203f370f876e75bf4759c3af36bbbe3cebd2c
matt werth <matt werth>
2017-05-05 12:14:30 +1000

    wiping marker contacts

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,8 +2,11 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
+	  	
+	  	Contact.all.find_by(code:'running').destroy
+	  	Contact.all.find_by(code:'clock').destroy
 	  	@output = Contact.all.where(code:'clock')
-	  	# Contact.all.find_by(code:'running').destroy
+	  	
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
 	end
 end
