761be9e9a8e19188227c195cd90391614ea63011
matt werth <matt werth>
2017-05-05 13:22:39 +1000

    hopefully fixed inactive customers in seed

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -14,8 +14,8 @@ class TestController < ApplicationController
 
 
 
-	  	# Contact.all.find_by(code:'running').destroy
-	  	# Contact.all.find_by(code:'clock').destroy
+	  	Contact.all.find_by(code:'running').destroy
+	  	Contact.all.find_by(code:'clock').destroy
 	  	# @output = Contact.all.where(code:'clock')
 
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
