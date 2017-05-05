4aa2c5666fe747497a6bc4e93ea90b3098ff8cb3
matt werth <matt werth>
2017-05-05 12:16:49 +1000

    not wiping marker contacts

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,8 +3,8 @@ class TestController < ApplicationController
 	
 	def index
 	  	
-	  	Contact.all.find_by(code:'running').destroy
-	  	Contact.all.find_by(code:'clock').destroy
+	  	# Contact.all.find_by(code:'running').destroy
+	  	# Contact.all.find_by(code:'clock').destroy
 	  	@output = Contact.all.where(code:'clock')
 	  	
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
