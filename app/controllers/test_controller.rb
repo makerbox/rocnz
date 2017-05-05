42ed4d1a7e0f6a69932f3f03866842b449e6505f
matt werth <matt werth>
2017-05-05 13:40:18 +1000

    inactive test again

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -7,15 +7,17 @@ class TestController < ApplicationController
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       @accounts = dbh.execute("SELECT * FROM customer_mastext").fetch(:all, :Struct)
       @accounts.each do |acct|
-        @output << (acct.InactiveCust.to_s + '--' + acct.Code.to_s)
+      	if acct.InactiveCust == 1
+        	@output << (acct.InactiveCust.to_s + '--' + acct.Code.to_s)
+    	end
       end
 
 
 
 
 
-	  	Contact.all.find_by(code:'running').destroy
-	  	Contact.all.find_by(code:'clock').destroy
+	  	# Contact.all.find_by(code:'running').destroy
+	  	# Contact.all.find_by(code:'clock').destroy
 	  	# @output = Contact.all.where(code:'clock')
 
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
