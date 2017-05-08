aeef92a8cbb8aa3c685c084c08687db10a38083e
matt werth <matt werth>
2017-05-08 11:36:36 +1000

    qty disc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,8 +21,8 @@ end
     # @output = `rails g migration AddSortToAccount sort`
     # system "git stash"
     system "git pull"
-    system "bundle exec rake db:migrate"
-    system "bundle exec rake routes"
+    # system "bundle exec rake db:migrate"
+    @output = `rake routes`
     # @output = `rake db:migrate`
     # system "bundle exec rake db:migrate"
     # system "rake db:migrate"
