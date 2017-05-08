47f0b5c370c40efdf233cd38cdd4ab7f3caef7df
matt werth <matt werth>
2017-05-08 15:06:41 +1000

    qty disc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
 
 def calc_qty_disc
-  qty = params[:qty]
+  @qty = params[:qty]
   # if current_user.mimic
   #   u = current_user.mimic.user
   # else
@@ -21,9 +21,9 @@ def calc_qty_disc
   # else
   #   # result = qty
   # end
-  respond_to do |format|
-    format.json { render json: qty }
-  end
+    respond_to do |format|
+       format.js { @qty }
+    end
 end
 
   def index
