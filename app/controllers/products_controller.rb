5deb2218ac2391d444a2eacde80673d26cd93a8f
matt werth <matt werth>
2017-05-08 13:18:34 +1000

    qty disc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,24 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   
-def calc_qty_disc(qty)
-  if current_user.mimic
-    u = current_user.mimic.user
-  else
-    u = current_user
-  end
-  prod_group = @product.group
-  prod_code = @product.code
-  price_cat = @product.pricecat
 
-  if Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
-    disco = Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
-    result = qty
-  else
-    result = qty
-  end
-  return result
-end
 
   def index
     availgroups = [] #create empty array to store the groups available to the current user
--- app/models/product.rb
+++ app/models/product.rb
@@ -24,6 +24,25 @@ def calc_discount(u, price, prod_group, prod_code, price_cat)
 end
 
 
+def calc_qty_disc(qty)
+  if current_user.mimic
+    u = current_user.mimic.user
+  else
+    u = current_user
+  end
+  prod_group = @product.group
+  prod_code = @product.code
+  price_cat = @product.pricecat
+
+  if Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+    disco = Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+    result = qty
+  else
+    result = qty
+  end
+  return result
+end
+
 
 
 end #end of class
