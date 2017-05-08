2cd331675343f1a793a649faec5cb22ee9410495
matt werth <matt werth>
2017-05-08 13:24:12 +1000

    qty disc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,25 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!
   # GET /products
   # GET /products.json
-  
 
+def calc_qty_disc(qty)
+  # if current_user.mimic
+  #   u = current_user.mimic.user
+  # else
+  #   u = current_user
+  # end
+  # prod_group = @product.group
+  # prod_code = @product.code
+  # price_cat = @product.pricecat
+
+  # if Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+  #   disco = Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+  #   result = qty
+  # else
+  #   result = qty
+  # end
+  return qty
+end
 
   def index
     availgroups = [] #create empty array to store the groups available to the current user
