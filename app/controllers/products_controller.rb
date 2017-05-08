db5c0dfa7ad3aef7ba4e6f3e89458d606c279380
matt werth <matt werth>
2017-05-08 15:51:09 +1000

    qty disc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,7 @@ def calc_qty_disc
   #   # result = qty
   # end
     respond_to do |format|
-        format.json { render json: @qty }
+        format.json { render json: @qty, status: :success }
     end
 end
 
