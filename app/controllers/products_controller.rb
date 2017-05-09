3601db5e4900719b9be027a1c6287196e7c950ae
matt werth <matt werth>
2017-05-09 14:07:40 +1000

    ajax qty works, just needs the right calc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,24 @@ def calc_qty_disc
   prod_group = params[:group]
   prod_code = params[:code]
   # price_cat = params[:pricecat]
-  if (current_user.has_role? :admin)
-    result = 10
+
+  if current_user.mimic
+    u = current_user.mimic.user
+  else
+    u = current_user
+  end
+  if u.account.discount
+    udisc = u.account.discount.strip
+  else
+    udisc = nil
+  end
+  if Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || udisc))
+    disco = Discount.all.find_by(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+    if disco.disctype == 'fixedtype'
+      result = price - disco.discount
+    else
+      result = price - ((price / 100) * disco.discount)
+    end
   else
     result = price
   end
