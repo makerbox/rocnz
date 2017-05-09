57a386027f1462ee4b377526ab4dc495e346e782
matt werth <matt werth>
2017-05-09 19:39:13 +1000

    wtf

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,8 @@ def calc_qty_disc
     u = current_user
   end
   
-  if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+  discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+  if discos.any?
     discos.each do |disco|
       # if disco.disctype == 'fixedtype'
       #   result = 'fixed'
