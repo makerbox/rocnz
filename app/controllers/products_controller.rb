3000ac3a0c15386591a40900c4a71976ab622148
matt werth <matt werth>
2017-05-15 17:44:44 +1000

    fixed discount seeder

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,13 +18,13 @@ def calc_qty_disc
   end
   
   discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip)) #get the matching discounts
-  discos.each do |disco| #loop through to find which maxqty applies
-    if qty <= disco.maxqty
-      @thedisco = disco #set @thedisco to the discount in which this qty fits
-    else
-      @thedisco = nil #otherwise jsut be nil if it doesn't fit or is above the top max qty
-    end
-  end
+  # discos.each do |disco| #loop through to find which maxqty applies
+  #   if qty <= disco.maxqty
+  #     @thedisco = disco #set @thedisco to the discount in which this qty fits
+  #   else
+  #     @thedisco = nil #otherwise jsut be nil if it doesn't fit or is above the top max qty
+  #   end
+  # end
 
   # if @thedisco
   #   result = price - @thedisco.discount
