f120713fd107bf465c293f33da0ec654747c9e61
matt werth <matt werth>
2017-05-16 18:53:35 +1000

    testing contacts again

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -9,6 +9,7 @@ class TestController < ApplicationController
 			if contact.Active == 1
 				account = Account.all.find_by(code: contact.Code.strip)
 				if account
+					account.skip_reconfirmation!
 					if account.user.update(email: contact.EmailAddress)
 						@results << account.user.email
 						@results << account.code
