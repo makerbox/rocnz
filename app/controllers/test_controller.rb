278b8f5f75b9627bec8e7bd68c9e292084853c81
matt werth <matt werth>
2017-05-16 11:55:04 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,8 +6,8 @@ class TestController < ApplicationController
           dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
           contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
           contacts.each do |contact|
-          	account = Account.all.find_by(code: contact.Code)
-          	@results << account.code
+          	# account = Account.all.find_by(code: contact.Code)
+          	@results << contact.Code
           end
 
           dbh.disconnect 
