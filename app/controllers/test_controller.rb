a441eb9052765f4ee79597c19bd24aa1115f16cb
matt werth <matt werth>
2017-05-16 15:34:12 +1000

    testing contacts again

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,15 +3,19 @@ class TestController < ApplicationController
 	
 	def index
 		@results = []
-          dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-          contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
-          contacts.each do |contact|
-          	if contact.Code.strip == 'MINGHY'
-          		@results << contact.EmailAddress
-          	end
-          end
-          dbh.disconnect 
+		dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+		contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
+		contacts.each do |contact|
+			if contact.Active
+				account = Account.all.find_by(code: contact.Code.strip)
+				if account
+					account.user.update_attributes(email: contact.EmailAddress)
+					@results << account.user.email
+				end
+			end
+		end
+		dbh.disconnect 
 
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
+	  end
 	end
-end
