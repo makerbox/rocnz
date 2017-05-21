0c0ced63c2715648787722a50173b503aa0c2cfb
matt werth <matt werth>
2017-05-21 19:20:31 +1000

    testing discounts

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,7 +4,37 @@ class TestController < ApplicationController
 	def index
 		@results = []
 		
-		@results = Discount.all
+		qty = 4
+  price = 4.8
+  prod_group = 'D'
+  prod_code = '633'
+  # price_cat = params[:pricecat]
+
+  if current_user.mimic
+    u = current_user.mimic.account.user
+  else
+    u = current_user
+  end
+  
+  if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip)) #get the matching discounts
+    discos.each do |disco| #loop through to find which maxqty applies
+      @result = number_with_precision(disco, precision: 2)
+      # if qty <= disco.maxqty
+      #   @thedisco = disco #set @thedisco to the discount in which this qty fits
+      # else
+      #   @thedisco = nil #otherwise jsut be nil if it doesn't fit or is above the top max qty
+      # end
+    end
+  else
+
+    # if @thedisco
+    #   result = price - @thedisco.discount
+    #   # need to also work for percentage discount
+    # else
+    #   result = price #should be discounted price
+    # end
+    @result = price
+  end
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
 	  end
 	end
