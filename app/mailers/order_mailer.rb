ef753ba2824fbde418b7dfc9278873454fea562f
matt werth <matt werth>
2017-05-22 09:19:31 +1000

    testing order

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,20 +1,20 @@
 class OrderMailer < ApplicationMailer
 		default from:'web@roccloudy.com'
 	
-	def order(order)
-		@thisorder = order
-		@account = order.user.account
-		subject = 'Roc Cloudy order ' + order.user.account.code.strip
-		mail(to: 'wholesaleorder@roccloudy.com', subject: subject)
-	end
+	# def order(order)
+	# 	@thisorder = order
+	# 	@account = order.user.account
+	# 	subject = 'Roc Cloudy order ' + order.user.account.code.strip
+	# 	mail(to: 'wholesaleorder@roccloudy.com', subject: subject)
+	# end
 	def receipt(order)
-		@thisorder = order
-		@account = order.user.account
+		# @thisorder = order
+		# @account = order.user.account
 		mail(to: 'mattwerth@mattwerth.com', subject: 'Roc Cloudy order')
 	end
-	def rep(order)
-		@thisorder = order
-		@account = order.user.account
-		mail(to: current_user.email, subject: 'Roc Cloudy order')
-	end
+	# def rep(order)
+	# 	@thisorder = order
+	# 	@account = order.user.account
+	# 	mail(to: current_user.email, subject: 'Roc Cloudy order')
+	# end
 end
