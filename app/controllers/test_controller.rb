956bebb2964a23d9f16fdafe358680cc1d426d1d
matt werth <matt werth>
2017-05-22 09:27:56 +1000

    testing order

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,8 +3,7 @@ class TestController < ApplicationController
 	
 	def index
 		OrderEmailJob.perform_async(Order.all.last)
-		@result = `heroku pg:credentials`
-		@result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
+		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
   end
 end
