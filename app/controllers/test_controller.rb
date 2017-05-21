cd91b66a7a761a6f126455b8b8b2c1e9c7715138
matt werth <matt werth>
2017-05-22 08:39:01 +1000

    tidying order email

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		Contact.destroy_all
+		@result = `heroku pg:credentials`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
   end
 end
--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -4,7 +4,7 @@ class OrderEmailJob
 
   # The perform method is in charge of our code execution when enqueued.
   def perform(order)
-  	  OrderMailer.order(order).deliver_now
+  	  # OrderMailer.order(order).deliver_now
   	  OrderMailer.receipt(order).deliver_now
   	  if current_user.has_role? :admin
   	  	OrderMailer.rep(order).deliver_now
