c3d417ec4f3d02f10965f4d4a6c964ec0d75c86d
matt werth <matt werth>
2017-05-22 09:52:10 +1000

    updated order mailer

--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -5,7 +5,7 @@ class OrderEmailJob
   # The perform method is in charge of our code execution when enqueued.
   def perform(order, user)
   	  # OrderMailer.order(order).deliver_now
-  	  OrderMailer.receipt(order).deliver_now
+  	  OrderMailer.receipt(order, user).deliver_now
   	  # if current_user.has_role? :admin
   	  # 	OrderMailer.rep(order).deliver_now
   	  # end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,10 @@
 class OrderMailer < ApplicationMailer
 		default from:'web@roccloudy.com'
 	
-	# def order(order)
+	# def order(order, user)
 	# 	@thisorder = order
 	# 	@account = order.user.account
+	# 	@user = user
 	# 	subject = 'Roc Cloudy order ' + order.user.account.code.strip
 	# 	mail(to: 'wholesaleorder@roccloudy.com', subject: subject)
 	# end
@@ -13,9 +14,10 @@ class OrderMailer < ApplicationMailer
 		@user = user
 		mail(to: 'mattwerth@mattwerth.com', subject: 'Roc Cloudy order')
 	end
-	# def rep(order)
+	# def rep(order, user)
 	# 	@thisorder = order
 	# 	@account = order.user.account
+	# 	@user = user
 	# 	mail(to: current_user.email, subject: 'Roc Cloudy order')
 	# end
 end
