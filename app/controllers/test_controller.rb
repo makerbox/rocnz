6dedd2f8124e6c738efa46625d8a1135ee9273b4
matt werth <matt werth>
2017-05-22 07:42:28 +1000

    wiping contacts

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,10 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-  qty = 4
-  prod_group = 'D'
-  prod_code = '1829DM'
-  # price_cat = params[:pricecat]
+		Contact.destroy_all
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
   end
 end
