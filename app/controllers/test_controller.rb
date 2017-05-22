d363f52f5720ac592e8769df0770950e51a6f1af
matt werth <matt werth>
2017-05-22 10:04:33 +1000

    updated order mailer

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		OrderEmailJob.new.perform(Order.all.last, current_user)
+		OrderEmailJob.new.perform(Order.all.last, current_user.id)
 		# OrderMailer.receipt(Order.all.last, current_user).deliver_now
 		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -4,6 +4,7 @@ class OrderEmailJob
 
   # The perform method is in charge of our code execution when enqueued.
   def perform(order, user)
+  	user = User.all.find_by(id: user)
   	  # OrderMailer.order(order, user).deliver_now
   	  OrderMailer.receipt(order, user).deliver_now
   	  # if current_user.has_role? :admin
