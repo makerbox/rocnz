7fe27714bfa1bcf410b87492e0d219752cf46fdb
matt werth <matt werth>
2017-05-22 10:03:05 +1000

    updated order mailer

--- app/jobs/order_email_job.rb
+++ app/jobs/order_email_job.rb
@@ -3,9 +3,9 @@ class OrderEmailJob
 	include SuckerPunch::Job
 
   # The perform method is in charge of our code execution when enqueued.
-  def perform(order)
+  def perform(order, user)
   	  # OrderMailer.order(order, user).deliver_now
-  	  OrderMailer.receipt(order, current_user).deliver_now
+  	  OrderMailer.receipt(order, user).deliver_now
   	  # if current_user.has_role? :admin
   	  # 	OrderMailer.rep(order, user).deliver_now
   	  # end
