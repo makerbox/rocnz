9dc01a1e282c1efd9e06313d80a0bc8c3656c027
matt werth <matt werth>
2017-05-22 13:57:06 +1000

    testing how many bad emails

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,18 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
+		@goodcount = 0
+		@badcount = 0
+		@results = []
+		@orders = User.all
+		@orders.each do |o|
+			if o.email.includes? '@'
+				@goodcount = goodcount + 1
+			else
+				@badcount = badcount + 1
+				@result << o.account.company
+			end
+		end
 		# OrderMailer.receipt(Order.all.last, current_user).deliver_now
 		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
