485f6d4533a8a4c5bb15eb6d7747ea0aa7e0669b
matt werth <matt werth>
2017-05-30 16:32:51 +1000

    testing roles in test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,18 +2,20 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		@goodcount = 0
-		@badcount = 0
-		@results = []
-		@users = User.all
-		@users.each do |u|
-			if u.email.include? '@'
-				@goodcount = @goodcount + 1
-			else
-				@badcount = @badcount + 1
-				@results << u.account
-			end
-		end
+		# @goodcount = 0
+		# @badcount = 0
+		# @results = []
+		# @users = User.all
+		# @users.each do |u|
+		# 	if u.email.include? '@'
+		# 		@goodcount = @goodcount + 1
+		# 	else
+		# 		@badcount = @badcount + 1
+		# 		@results << u.account
+		# 	end
+		# end
+
+		@roles = UserRoles.all
 		# OrderMailer.receipt(Order.all.last, current_user).deliver_now
 		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
