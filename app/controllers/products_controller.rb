adcf22ad7690a790ec843bea2858c2ce430b0569
matt werth <matt werth>
2017-05-30 16:04:34 +1000

    hide products buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
 
-def hide(product)
+def hide
+  product = Product.all.find_by(id: params[:id])
   if product.hidden
     product.update_attributes(hidden: false)
   else
