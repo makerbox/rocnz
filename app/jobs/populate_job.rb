6833ae32b4b17e6f72e2aa491ea15fea4b029377
matt werth <matt werth>
2017-05-30 15:00:44 +1000

    testing without email seed

--- app/jobs/populate_job.rb
+++ app/jobs/populate_job.rb
@@ -250,18 +250,18 @@ class PopulateJob
       dbh.disconnect 
 
       # --------------------- ADD EMAIL ADDRESSES ----------------------
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
-      contacts.each do |contact|
-        if contact.Active == 1
-          if account = Account.all.find_by(code: contact.Code.strip)
-            if !User.all.find_by(email: contact.EmailAddress)
-              account.user.update_attribute(email: contact.EmailAddress)
-            end
-          end
-        end
-      end
-      dbh.disconnect 
+      # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+      # contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
+      # contacts.each do |contact|
+      #   if contact.Active == 1
+      #     if account = Account.all.find_by(code: contact.Code.strip)
+      #       if !User.all.find_by(email: contact.EmailAddress)
+      #         account.user.update_attribute(email: contact.EmailAddress)
+      #       end
+      #     end
+      #   end
+      # end
+      # dbh.disconnect 
 
 
 
