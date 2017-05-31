f8fa330d4db37b8bf89edf08395e505e9e85be7a
matt werth <matt werth>
2017-05-31 11:03:17 +1000

    added brands to account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -141,6 +141,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:user_id, :website, :rep, :code, :company, :street, :suburb, :postcode, :state, :country, :phone, :contact, :seller_level, :sort, :discount)
+      params.require(:account).permit(:brands, :user_id, :website, :rep, :code, :company, :street, :suburb, :postcode, :state, :country, :phone, :contact, :seller_level, :sort, :discount)
     end
 end
