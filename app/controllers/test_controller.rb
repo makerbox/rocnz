511237b22b9fd1b06c4309ee3d08019b2d7f487e
matt werth <matt werth>
2017-06-01 07:06:08 +1000

    display date in sidecart

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -21,9 +21,9 @@ class TestController < ApplicationController
       contacts.each do |contact|
         if contact.Active == 1
           if account = Account.all.find_by(code: contact.Code.strip)
-            # if !User.all.find_by(email: contact.EmailAddress)
-            #   account.user.update_attribute(email: contact.EmailAddress)
-            # end
+            if !User.all.find_by(email: contact.EmailAddress)
+              account.user.update_attribute(email: contact.EmailAddress)
+            end
           end
         end
       end
