e424b0f6f0551a9f684fd13553d0860c2cafd8aa
matt werth <matt werth>
2017-05-31 21:04:49 +1000

    testing address issue

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -15,10 +15,11 @@ class TestController < ApplicationController
 		# 	end
 		# end
 
-		@products = Product.all
-		@products.each do |prod|
-			# prod.update_attributes(hidden: false)
-		end
+		@results = []
+		@customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+      @customers.each do |c|
+      	@results << c.Street
+      end
 		# OrderMailer.receipt(Order.all.last, current_user).deliver_now
 		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
 	  	# system "heroku pg:push development postgresql-round-86328 --app shrouded-waters-74068"
