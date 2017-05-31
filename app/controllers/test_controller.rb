026982530bb35972a1fad3f2e74b4fb55ab9912a
matt werth <matt werth>
2017-06-01 06:57:10 +1000

    display date in sidecart

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -15,27 +15,6 @@ class TestController < ApplicationController
 		# 	end
 		# end
 
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-      @customers.each do |c|
-        code = c.Code.strip
-        if Account.all.find_by(code: code)
-          account = Account.all.find_by(code: code)
-          compname = c.Name
-          street = c.Street
-          suburb = c.Suburb 
-          state = c.Territory
-          postcode = c.Postcode 
-          phone = c.Phone 
-          sort = c.Sort 
-          discount = c.SpecialPriceCat 
-          seller_level = c.PriceCat
-          rep = c.SalesRep
-          account.update_attributes(approved: 'approved', phone: phone, street: street, state: state, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
-        end
-      end
-      dbh.disconnect 
-
       # --------------------- ADD EMAIL ADDRESSES ----------------------
       dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
       contacts = dbh.execute("SELECT * FROM contact_details_file").fetch(:all, :Struct)
@@ -49,48 +28,6 @@ class TestController < ApplicationController
         end
       end
       dbh.disconnect 
-
-
-      #-------------------------- CREATE ADMIN USER -------------------------------------
-      if adminuser = User.all.find_by(email: 'web@roccloudy.com')
-        adminuser.add_role :admin
-        adminuser.remove_role :user
-        if adminuser.account
-          adminuser.account.update_attributes(approved: 'approved', sort: 'U/L/R/P')
-        else
-          Account.create(code: 'ADMIN', company: 'Roc', user: adminuser, sort: 'U/L/R/P')
-        end
-      else
-        adminuser = User.new(email: 'web@roccloudy.com', password:'cloudy_16', password_confirmation: 'cloudy_16')
-        adminuser.add_role :admin
-        adminuser.save(validate: false)
-        Account.create(code: 'ADMIN', company: 'Roc', user: adminuser, sort: 'U/L/R/P')
-      end
-
-      #-------------------------- CREATE REP ACCOUNTS -----------------------------------
-      def createrep(repemail, repcode)
-        if repuser = User.all.find_by(email: repemail)
-          repuser.add_role :admin
-          repuser.remove_role :user
-          if repuser.account
-            repuser.account.update_attributes(approved: 'approved', sort: 'U/L/R/P')
-          else
-            Account.create(code: repcode, company: 'Roc', user: repuser, sort: 'U/L/R/P')
-          end
-        else
-          repuser = User.new(email: repemail, password:'cloudy_16', password_confirmation: 'cloudy_16')
-          repuser.add_role :admin
-          repuser.save(validate: false)
-          Account.create(code: repcode, company: 'Roc', user: repuser, sort: 'U/L/R/P')
-        end
-      end
-
-      createrep('nsw@roccloudy.com', 'REPNSW')
-      createrep('vic@roccloudy.com', 'REPVIC')
-      createrep('qld1@roccloudy.com', 'REPQLD1')
-      createrep('qld2@roccloudy.com', 'REPQLD2')
-      createrep('nz@roccloudy.com', 'REPNZ')
-      createrep('office@roccloudy.com', 'ADMINOFFICE')
 
 		# OrderMailer.receipt(Order.all.last, current_user).deliver_now
 		# @result = `heroku db:push [postgres://bpupvrcqomwfwk:55tz1h8GUNGOyWVTkWFjAttzY7@ec2-54-225-244-221.compute-1.amazonaws.com:5432/de53vgd0mccdbt]`
