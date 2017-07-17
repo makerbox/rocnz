620bf3e212e709eca07862853eccd9f7598b5361
makerbox <mattwerth@mattwerth.com>
2017-07-17 11:06:16 +1000

    testing discounts on local server

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -18,8 +18,9 @@ class TestController < ApplicationController
 		@result = ''
 
 
-    @discounts = Discount.all
-
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+    @discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
+    dbh.disconnect
 
   end
 end
