1ad72e5f5c0a301c539a9aa572773d19db6411bc
makerbox <mattwerth@mattwerth.com>
2017-07-24 13:40:42 +1000

    discounts are good just need to match price cat - testing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,51 @@
 
 
       # -------------------------GET PRODUCTS AND CREATE / UPDATE PRODUCT RECORDS------------------------
-      # @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-      #     @products.each do |p|
-      #       if p.Inactive == 0
-      #         code = p.Code.strip
-      #         description = p.Description.to_s.strip
-      #         price1 = p.SalesPrice1
-      #         price2 = p.SalesPrice2
-      #         price3 = p.SalesPrice3
-      #         price4 = p.SalesPrice4
-      #         price5 = p.SalesPrice5
-      #         rrp = p.SalesPrice6
-      #         qty = p.QtyInStock
-      #         if p.AllowDisc == 1
-      #           allow_disc = true
-      #         else
-      #           allow_disc = false
-      #         end
-      #         group = p.ProductGroup.to_s.strip
-      #         pricecat = p.PriceCat.to_s.strip
-      #         # # needs category
-      #         if Product.all.where(code: code).exists?
-      #           Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
-      #           filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-      #           if File.exist?(filename)
-      #             # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-      #             # stop from overloading transformations
-      #           else
-      #             Product.all.find_by(code: code).destroy
-      #           end
-      #         else
-      #           newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
-      #           filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
-      #           if File.exist?(filename)
-      #             Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
-      #             # stop from overloading transformations
-      #             newproduct.save
-      #           end
+      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+          @products.each do |p|
+            if p.Inactive == 0
+              puts p.SpecialPriceCat
+              
+              # code = p.Code.strip
+              # description = p.Description.to_s.strip
+              # price1 = p.SalesPrice1
+              # price2 = p.SalesPrice2
+              # price3 = p.SalesPrice3
+              # price4 = p.SalesPrice4
+              # price5 = p.SalesPrice5
+              # rrp = p.SalesPrice6
+              # qty = p.QtyInStock
+              # if p.AllowDisc == 1
+              #   allow_disc = true
+              # else
+              #   allow_disc = false
+              # end
+              # group = p.ProductGroup.to_s.strip
+              # pricecat = p.PriceCat.to_s.strip
+              # # # needs category
+              # if Product.all.where(code: code).exists?
+              #   Product.all.find_by(code: code).update_attributes(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty)
+              #   filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
+              #   if File.exist?(filename)
+              #     # Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+              #     # stop from overloading transformations
+              #   else
+              #     Product.all.find_by(code: code).destroy
+              #   end
+              # else
+              #   newproduct = Product.new(allow_disc: allow_disc, pricecat: pricecat, group: group, code: code, description: description, price1: price1, price2: price2, price3: price3, price4: price4, price5: price5, rrp: rrp, qty: qty, hidden: false)
+              #   filename = "E:\\Attache\\Attache\\Roc\\Images\\Product\\" + code + ".jpg"
+              #   if File.exist?(filename)
+              #     Cloudinary::Uploader.upload(filename, :public_id => code, :overwrite => true)
+              #     # stop from overloading transformations
+              #     newproduct.save
+              #   end
                 
-      #         end
-      #       end
-      #     end
+              # end
+            end
+          end
 
-      #     dbh.disconnect
+          dbh.disconnect
 
       #-------------------------UPDATE PRODUCTS WITH CATEGORIES -------------------------------------
       # dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
@@ -96,109 +98,109 @@
       # dbh.disconnect
 
       # ------------------------DISCOUNTS---------------------------------------------------------
-       Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
-          dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-          discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
+       # Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
+       #    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
+       #    discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
 
-          def disco(percentage, fixed, fixedprice, level, maxqty, ctype, ptype, cust, prod)
-            if fixedprice == 9 #if the discount is a fixed price
-              disctype = 'fixedtype'
-              discount = fixed
-              if ctype == 10
-                customertype = 'code_fixed'
-              else
-                customertype = 'group_fixed'
-              end
-              if ptype == 10
-                producttype = 'code_fixed'
-              elsif ptype == 30
-                producttype = 'group_fixed'
-              else
-                producttype = 'cat_fixed'
-              end
-            else
-              disctype = 'percentagetype'
-              discount = percentage
-              if ctype == 10
-                customertype = 'code_percent'
-              else
-                customertype = 'group_percent'
-              end
-              if ptype == 10
-                producttype = 'code_percent'
-              elsif ptype == 30
-                producttype = 'group_percent'
-              else
-                producttype = 'cat_percent'
-              end
-            end
-            if maxqty #sometimes there is no qty
-              if maxqty >= 10000 #sometimes it's way too big to store as an integer
-                maxqty = 9999
-              end
-            end
-            if !prod.nil? && !cust.nil?
-              puts 'DISCOUNT------------------------------'
-              puts cust.strip
-              discount = sprintf("%.2f", discount)
-              puts discount
-              Discount.create(customertype: customertype, producttype: producttype, customer: cust.strip, product: prod.strip, discount: discount, level: level, maxqty: maxqty, disctype: disctype)
-            end
+       #    def disco(percentage, fixed, fixedprice, level, maxqty, ctype, ptype, cust, prod)
+       #      if fixedprice == 9 #if the discount is a fixed price
+       #        disctype = 'fixedtype'
+       #        discount = fixed
+       #        if ctype == 10
+       #          customertype = 'code_fixed'
+       #        else
+       #          customertype = 'group_fixed'
+       #        end
+       #        if ptype == 10
+       #          producttype = 'code_fixed'
+       #        elsif ptype == 30
+       #          producttype = 'group_fixed'
+       #        else
+       #          producttype = 'cat_fixed'
+       #        end
+       #      else
+       #        disctype = 'percentagetype'
+       #        discount = percentage
+       #        if ctype == 10
+       #          customertype = 'code_percent'
+       #        else
+       #          customertype = 'group_percent'
+       #        end
+       #        if ptype == 10
+       #          producttype = 'code_percent'
+       #        elsif ptype == 30
+       #          producttype = 'group_percent'
+       #        else
+       #          producttype = 'cat_percent'
+       #        end
+       #      end
+       #      if maxqty #sometimes there is no qty
+       #        if maxqty >= 10000 #sometimes it's way too big to store as an integer
+       #          maxqty = 9999
+       #        end
+       #      end
+       #      if !prod.nil? && !cust.nil?
+       #        puts 'DISCOUNT------------------------------'
+       #        puts cust.strip
+       #        discount = sprintf("%.2f", discount)
+       #        puts discount
+       #        Discount.create(customertype: customertype, producttype: producttype, customer: cust.strip, product: prod.strip, discount: discount, level: level, maxqty: maxqty, disctype: disctype)
+       #      end
 
-          end
+       #    end
 
-          discounts.each do |d|
-            if (d.LevelNum >= 1) 
-              percentage = d.DiscPerc1
-              fixed = d.Price1
-              fixedprice = d.PriceCode1
-              level = 1
-              maxqty = d.MaxQty1
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 2) 
-              percentage = d.DiscPerc2
-              fixed = d.Price2
-              fixedprice = d.PriceCode2
-              level = 2
-              maxqty = d.MaxQty2
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 3) 
-              percentage = d.DiscPerc3
-              fixed = d.Price3
-              fixedprice = d.PriceCode3
-              level = 3
-              maxqty = d.MaxQty3
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 4) 
-              percentage = d.DiscPerc4
-              fixed = d.Price4
-              fixedprice = d.PriceCode4
-              level = 4
-              maxqty = d.MaxQty4
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 5) 
-              percentage = d.DiscPerc5
-              fixed = d.Price5
-              fixedprice = d.PriceCode5
-              level = 5
-              maxqty = d.MaxQty5
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-            if (d.LevelNum >= 6) 
-              percentage = d.DiscPerc6
-              fixed = d.Price6
-              fixedprice = d.PriceCode6
-              level = 6
-              maxqty = d.MaxQty6
-              disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-            end
-          end
+       #    discounts.each do |d|
+       #      if (d.LevelNum >= 1) 
+       #        percentage = d.DiscPerc1
+       #        fixed = d.Price1
+       #        fixedprice = d.PriceCode1
+       #        level = 1
+       #        maxqty = d.MaxQty1
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #      if (d.LevelNum >= 2) 
+       #        percentage = d.DiscPerc2
+       #        fixed = d.Price2
+       #        fixedprice = d.PriceCode2
+       #        level = 2
+       #        maxqty = d.MaxQty2
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #      if (d.LevelNum >= 3) 
+       #        percentage = d.DiscPerc3
+       #        fixed = d.Price3
+       #        fixedprice = d.PriceCode3
+       #        level = 3
+       #        maxqty = d.MaxQty3
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #      if (d.LevelNum >= 4) 
+       #        percentage = d.DiscPerc4
+       #        fixed = d.Price4
+       #        fixedprice = d.PriceCode4
+       #        level = 4
+       #        maxqty = d.MaxQty4
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #      if (d.LevelNum >= 5) 
+       #        percentage = d.DiscPerc5
+       #        fixed = d.Price5
+       #        fixedprice = d.PriceCode5
+       #        level = 5
+       #        maxqty = d.MaxQty5
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #      if (d.LevelNum >= 6) 
+       #        percentage = d.DiscPerc6
+       #        fixed = d.Price6
+       #        fixedprice = d.PriceCode6
+       #        level = 6
+       #        maxqty = d.MaxQty6
+       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
+       #      end
+       #    end
 
-          dbh.disconnect 
+       #    dbh.disconnect 
 
       # -------------------------GET CUSTOMERS AND ADD / UPDATE THE DB----------------------------------
 
