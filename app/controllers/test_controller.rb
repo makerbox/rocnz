c0cc9c2e39cb5c9123d21ec8681148391b053b02
makerbox <mattwerth@mattwerth.com>
2017-07-24 16:28:56 +1000

    heroku

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,137 +2,30 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		# @goodcount = 0
-		# @badcount = 0
-		# @results = []
-		# @users = User.all
-		# @users.each do |u|
-		# 	if u.email.include? '@'
-		# 		@goodcount = @goodcount + 1
-		# 	else
-		# 		@badcount = @badcount + 1
-		# 		@results << u.account
-		# 	end
-		# end
-		@results = []
-		@result = ''
-		
-		# ------------------------DISCOUNTS---------------------------------------------------------
-       # Discount.destroy_all #wipe existing discounts in case of some deletions in Attache
-       #    dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-       #    discounts = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
+       end
+       def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 
-       #    def disco(percentage, fixed, fixedprice, level, maxqty, ctype, ptype, cust, prod)
-       #      if fixedprice == 9 #if the discount is a fixed price
-       #        disctype = 'fixedtype'
-       #        discount = fixed
-       #        if ctype == 10
-       #          customertype = 'code_fixed'
-       #        else
-       #          customertype = 'group_fixed'
-       #        end
-       #        if ptype == 10
-       #          producttype = 'code_fixed'
-       #        elsif ptype == 30
-       #          producttype = 'group_fixed'
-       #        else
-       #          producttype = 'cat_fixed'
-       #        end
-       #      else
-       #        disctype = 'percentagetype'
-       #        discount = percentage
-       #        if ctype == 10
-       #          customertype = 'code_percent'
-       #        else
-       #          customertype = 'group_percent'
-       #        end
-       #        if ptype == 10
-       #          producttype = 'code_percent'
-       #        elsif ptype == 30
-       #          producttype = 'group_percent'
-       #        else
-       #          producttype = 'cat_percent'
-       #        end
-       #      end
-       #      if maxqty #sometimes there is no qty
-       #        if maxqty >= 10000 #sometimes it's way too big to store as an integer
-       #          maxqty = 9999
-       #        end
-       #      end
-       #      if !prod.nil? && !cust.nil?
-       # Product.all.each do |disco|
-       	# @results << disco.pricecat
-       	# @results << '+'
-       	# @results << disco.product
-       	# @results << '='
-       	# @results << disco.discount
-       	# @results << '----'
-       	# @results << disco.disctype
-       	# @results << '-------------------------'
-              
-       # end
-       #      	@results << cust.strip
-       #      	@results << '='
-       #      	@results << discount
-       #      	@results << '-----------------------------'
-       #        # Discount.create(customertype: customertype, producttype: producttype, customer: cust.strip, product: prod.strip, discount: discount, level: level, maxqty: maxqty, disctype: disctype)
-       #      end
+       if u.account.discount
+              udisc = u.account.discount.strip
+       else
+              udisc = nil
+       end
 
-       #    end
+       
+       if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
+              if disco = discos.where('maxqty > ?', qty).first
+                  if disco.disctype == 'fixedtype'
+                    result =  price - disco.discount
+                  else
+                    result = price - ((price / 100) * disco.discount)
+                  end
+              else
+                     result = price
+              end
+       else
+              result = price
+       end
 
-       #    discounts.each do |d|
-       #      if (d.LevelNum >= 1) 
-       #        percentage = d.DiscPerc1
-       #        fixed = d.Price1
-       #        fixedprice = d.PriceCode1
-       #        level = 1
-       #        maxqty = d.MaxQty1
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #      if (d.LevelNum >= 2) 
-       #        percentage = d.DiscPerc2
-       #        fixed = d.Price2
-       #        fixedprice = d.PriceCode2
-       #        level = 2
-       #        maxqty = d.MaxQty2
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #      if (d.LevelNum >= 3) 
-       #        percentage = d.DiscPerc3
-       #        fixed = d.Price3
-       #        fixedprice = d.PriceCode3
-       #        level = 3
-       #        maxqty = d.MaxQty3
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #      if (d.LevelNum >= 4) 
-       #        percentage = d.DiscPerc4
-       #        fixed = d.Price4
-       #        fixedprice = d.PriceCode4
-       #        level = 4
-       #        maxqty = d.MaxQty4
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #      if (d.LevelNum >= 5) 
-       #        percentage = d.DiscPerc5
-       #        fixed = d.Price5
-       #        fixedprice = d.PriceCode5
-       #        level = 5
-       #        maxqty = d.MaxQty5
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #      if (d.LevelNum >= 6) 
-       #        percentage = d.DiscPerc6
-       #        fixed = d.Price6
-       #        fixedprice = d.PriceCode6
-       #        level = 6
-       #        maxqty = d.MaxQty6
-       #        disco(percentage, fixed, fixedprice, level, maxqty, d.CustomerType, d.ProductType, d.Customer, d.Product)
-       #      end
-       #    end
-
-       #    dbh.disconnect 
-
-
-  end
+       return result
+end
 end
