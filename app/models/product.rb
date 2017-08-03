096d5e652199012308892cf6184a0be6416692b5
makerbox <mattwerth@mattwerth.com>
2017-08-03 17:57:08 +1000

    testing discounts

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,17 +12,17 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 
 	
 	if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
-		if disco = discos.where('maxqty >= ?', qty).first
+		if disco = discos.where('maxqty >= ?', 1).first
 		    if disco.disctype == 'fixedtype'
 		      result =  disco.discount
 		    else
 		      result = price - ((price / 100) * disco.discount)
 		    end
 		else
-			result = 1
+			result = price
 		end
 	else
-		result = 2
+		result = price
 	end
 
 	return result
