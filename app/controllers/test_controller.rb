dff10a88ae173b09a52d49be96aa0136f3f5352d
makerbox <mattwerth@mattwerth.com>
2017-08-03 21:22:26 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,30 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-       end
-       def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
-
-       if u.account.discount
-              udisc = u.account.discount.strip
-       else
-              udisc = nil
-       end
-
-       
-       if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code.strip || u.account.discount.strip))
-              if disco = discos.where('maxqty > ?', qty).first
-                  if disco.disctype == 'fixedtype'
-                    result =  price - disco.discount
-                  else
-                    result = price - ((price / 100) * disco.discount)
-                  end
-              else
-                     result = price
-              end
-       else
-              result = price
-       end
-
-       return result
+       @results = Discount.all.where(product: ('PM'), customer: (current_user.mimic.account.code.strip || current_user.mimic.account.discount))
 end
 end
