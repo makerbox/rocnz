435b7d65ae9384b1bc9c2a1a5f6b834a8deac829
makerbox <mattwerth@mattwerth.com>
2017-08-03 21:30:24 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-       @results = Discount.all.where(product: ('PM'), customer: (current_user.mimic.account.code.strip || current_user.mimic.account.discount))
+       @results = Discount.all.where(product: ('PM'), customer: (current_user.mimic.account.code.strip || current_user.mimic.account.discount)).first
 end
 end
