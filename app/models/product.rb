170f824841782ae9f057ae761a1066e61eadf646
makerbox <mattwerth@mattwerth.com>
2017-08-03 22:22:55 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,22 +4,22 @@ has_many :orders, through: :quantities
 
 def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 
-	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: ('JOHN LUK' || u.account.discount))
-		discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: ('JOHN LUK' || u.account.discount))
-		# if discos.where('maxqty >= ?', qty).first
-			disco = discos.first
-		    # if disco.disctype == 'fixedtype'
+	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
+		discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
+		if discos.where('maxqty >= ?', qty).first
+			disco = discos.where('maxqty >= ?', qty).first
+		    if disco.disctype == 'fixedtype'
 		      result =  disco.discount
-		    # else
-		    #   result = price - ((price / 100) * disco.discount)
-		    # end
-		# else
-		# 	result = 1
-		# end
+		    else
+		      result = price - ((price / 100) * disco.discount)
+		    end
+		else
+			result = 1
+		end
 	else
 		result = 2
 	end
-
+	result = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount)).first.discount
 	return result
 end
 
