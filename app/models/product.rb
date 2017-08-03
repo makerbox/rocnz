da03b55d4ec628cdcb224fae09a10d9f3bcda6c5
makerbox <mattwerth@mattwerth.com>
2017-08-04 09:59:01 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,8 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 	
 	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
 		# FOR SOME REASON CHANGING THE ORDER OF THE 'price_cat || prod_code || prod_group' WORKS? THIS COULD MEAN SOMETHING ELSE IS WRONG
-		discos = Discount.all.where(product: price_cat || prod_code || prod_group, customer: (u.account.code || u.account.discount))
+		discos = Discount.all.where(product: price_cat , customer: u.account.code) || Discount.all.where(product: prod_code , customer: u.account.code) || Discount.all.where(product: prod_group , customer: u.account.code) || Discount.all.where(product: price_cat , customer: u.account.discount) || Discount.all.where(product: prod_code , customer: u.account.discount) || Discount.all.where(product: prod_group , customer: u.account.discount)
+
 		if discos.where('maxqty >= ?', qty).first
 			disco = discos.where('maxqty >= ?', qty).first
 		    if disco.disctype == 'fixedtype'
