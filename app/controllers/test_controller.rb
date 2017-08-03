ec2f3d8fb93689ee7e3091afbbb206d83b2614be
makerbox <mattwerth@mattwerth.com>
2017-08-03 21:33:48 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-       discos = Discount.all.where(product: ('PM'), customer: (current_user.mimic.account.code.strip || current_user.mimic.account.discount)).first
+       discos = Discount.all.where(product: ('PM'), customer: (current_user.mimic.account.code.strip || current_user.mimic.account.discount))
        @results = discos.where('maxqty >= ?', 1).first
 end
 end
