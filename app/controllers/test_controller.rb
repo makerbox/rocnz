2d471d6fe17e296779bf3d4446516c7a279c41ad
makerbox <mattwerth@mattwerth.com>
2017-08-03 21:04:23 +1000

    testing

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,13 +6,12 @@ class TestController < ApplicationController
     @results = []
 
 dbh = RDBI.connect :ODBC, :db => "wholesaleportal"
-      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+      @customers = dbh.execute("SELECT * FROM product_special_prices").fetch(:all, :Struct)
       @customers.each do |c|
-          @results << c.PriceCat
-          @results << '-cat-----'
-          @results << c.Name
-          @results << '-name-----'
-          @results << c.SpecialPriceCat
+          @results << c.CustomerType
+          @results << '<------>'
+          @results << c.Customer
+          @results << '------||'
       end
       dbh.disconnect 
 
