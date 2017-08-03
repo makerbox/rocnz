101e76830d83fe4cc22176e230dce7af7029772d
makerbox <mattwerth@mattwerth.com>
2017-08-03 22:44:59 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ has_many :orders, through: :quantities
 def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 
 	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
-		discos = Discount.all.where(product: (price_cat), customer: ('JOHN LUK'))
+		discos = Discount.all.where(product: (price_cat), customer: (u.account.code))
 		if discos.where('maxqty >= ?', qty).first
 			disco = discos.where('maxqty >= ?', qty).first
 		    if disco.disctype == 'fixedtype'
