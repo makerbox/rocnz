ef558cbf161e99aab47e7606763f401b0251bbfe
makerbox <mattwerth@mattwerth.com>
2017-08-03 22:59:03 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,11 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 
 	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
 		discos = Discount.all.where(product: (prod_group), customer: (u.account.code))
-		discos << Discount.all.where(product: (prod_code), customer: (u.account.code))
-		discos << Discount.all.where(product: (price_cat), customer: (u.account.code))
-		discos << Discount.all.where(product: (prod_group), customer: (u.account.discount))
-		discos << Discount.all.where(product: (prod_code), customer: (u.account.discount))
-		discos << Discount.all.where(product: (price_cat), customer: (u.account.discount))		
+		# discos << Discount.all.where(product: (prod_code), customer: (u.account.code))
+		# discos << Discount.all.where(product: (price_cat), customer: (u.account.code))
+		# discos << Discount.all.where(product: (prod_group), customer: (u.account.discount))
+		# discos << Discount.all.where(product: (prod_code), customer: (u.account.discount))
+		# discos << Discount.all.where(product: (price_cat), customer: (u.account.discount))		
 		if discos.where('maxqty >= ?', qty).first
 			disco = discos.where('maxqty >= ?', qty).first
 		    if disco.disctype == 'fixedtype'
