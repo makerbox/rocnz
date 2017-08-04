12b91aaf1176bd41263453aa333e3f6c550eb146
makerbox <mattwerth@mattwerth.com>
2017-08-04 10:04:07 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,9 @@ has_many :orders, through: :quantities
 
 def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 	
-	if Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
+	if discos = Discount.all.where(product: (prod_group || prod_code || price_cat), customer: (u.account.code || u.account.discount))
 		# FOR SOME REASON CHANGING THE ORDER OF THE 'price_cat || prod_code || prod_group' WORKS? THIS COULD MEAN SOMETHING ELSE IS WRONG
-		discos = (Discount.all.where(product: price_cat , customer: u.account.code)) || (Discount.all.where(product: prod_code , customer: u.account.code)) || (Discount.all.where(product: prod_group , customer: u.account.code)) || (Discount.all.where(product: price_cat , customer: u.account.discount)) || (Discount.all.where(product: prod_code , customer: u.account.discount)) || (Discount.all.where(product: prod_group , customer: u.account.discount))
+		# discos = (Discount.all.where(product: price_cat , customer: u.account.code)) || (Discount.all.where(product: prod_code , customer: u.account.code)) || (Discount.all.where(product: prod_group , customer: u.account.code)) || (Discount.all.where(product: price_cat , customer: u.account.discount)) || (Discount.all.where(product: prod_code , customer: u.account.discount)) || (Discount.all.where(product: prod_group , customer: u.account.discount))
 
 		if discos.where('maxqty >= ?', qty).first
 			disco = discos.where('maxqty >= ?', qty).first
@@ -20,6 +20,15 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 		end
 	else
 		result = price
+	end
+	if prod_group
+		result = 1
+	end
+	if prod_code
+		result = 2
+	end
+	if price_cat
+		result = 3
 	end
 	return result
 end
