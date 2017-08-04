d427a9e6eb59644b85daef14dd07654043560151
makerbox <mattwerth@mattwerth.com>
2017-08-04 11:23:42 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,7 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 		puts '--------------------------------------------'
 		puts '--------------------------------------------'
 		puts '--------------------------------------------'
-		puts discos.all.where('maxqty >= ?', qty).first	
+		puts discos.all.where('maxqty >= ?', qty).first.discount	
 		    if disco.disctype == 'fixedtype'
 		      result =  disco.discount
 		    else
