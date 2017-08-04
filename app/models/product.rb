5783c2248684c32b4b8128f1ed574365adb86393
makerbox <mattwerth@mattwerth.com>
2017-08-04 11:08:27 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,11 +8,11 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 		# FOR SOME REASON CHANGING THE ORDER OF THE 'price_cat || prod_code || prod_group' WORKS? THIS COULD MEAN SOMETHING ELSE IS WRONG
 		# discos = (Discount.all.where(product: price_cat , customer: u.account.code)) || (Discount.all.where(product: prod_code , customer: u.account.code)) || (Discount.all.where(product: prod_group , customer: u.account.code)) || (Discount.all.where(product: price_cat , customer: u.account.discount)) || (Discount.all.where(product: prod_code , customer: u.account.discount)) || (Discount.all.where(product: prod_group , customer: u.account.discount))
 	if discos = Discount.all.where('product = ? OR product = ? OR product = ?', price_cat , prod_code , prod_group).where('customer = ? OR customer = ?', u.account.code, u.account.discount)
-		if disco = discos.where('maxqty >= ?', 1).first			
+		if disco = discos.all.where('maxqty >= ?', 1).first			
 		    if disco.disctype == 'fixedtype'
 		      result =  disco.discount
 		    else
-		      result = 1
+		      result = price - ((price / 100) * disco.discount)
 		    end
 		else
 			result = price
