b1f81f27592ff22d5ff8a838c71c37c28e4e5181
makerbox <mattwerth@mattwerth.com>
2017-08-04 10:12:32 +1000

    testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,13 +21,13 @@ def calc_discount(u, price, prod_group, prod_code, price_cat, qty)
 	else
 		result = price
 	end
-	if prod_group
+	if discos = Discount.all.where(product: prod_group, customer: u.account.code)
 		result = 1
 	end
-	if prod_code
+	if discos = Discount.all.where(product: prod_code, customer: u.account.code)
 		result = 2
 	end
-	if price_cat
+	if discos = Discount.all.where(product: price_cat, customer: u.account.code)
 		result = 3
 	end
 	return result
