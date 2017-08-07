6585b3ec84eff222cbd27346e4dc174d7dd7ee19
makerbox <mattwerth@mattwerth.com>
2017-08-07 14:20:23 +1000

    removed last two migrations to do manually instead

--- db/migrate/20170724024618_remove_discount_from_discounts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDiscountFromDiscount < ActiveRecord::Migration
-  def change
-    remove_column :discount, :discount, :decimal
-  end
-end
--- db/migrate/20170724025031_add_discount_to_discounts.rb
+++
@@ -1,5 +0,0 @@
-class AddDiscountToDiscount < ActiveRecord::Migration
-  def change
-    add_column :discount, :discount, :float
-  end
-end
