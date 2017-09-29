1700e57d32c9eabeee1b4e8d74ca99925fefa4d3
makerbox <mattwerth@mattwerth.com>
2017-09-29 12:40:15 +1000

    testing kfi method

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,23 +2,6 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		@results = []
-      dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
-      @products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-      @categories = dbh.execute("SELECT * FROM prodmastext").fetch(:all, :Struct)
-      @categories.each do |cat|
-            if cat.CostCentre #if the prodmastext record has a category, then let's do it
-              categorycode = cat.Code.strip
-              if Product.find_by(code: categorycode) #if the product exists, let's give it the category (some products without images have no dice)
-                # Product.find_by(code: categorycode).update_attributes(category: cat.CostCentre.strip)
-              	@results << "category code - "
-              	@results << categorycode
-              	@results << " + cost centre - "
-              	@results << cat.CostCentre
-              	@results << "------------------------"
-              end
-            end
-          end
-          dbh.disconnect
+		@order.last.create_kfi
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :quantities,  dependent: :destroy 
   has_many :products, through: :quantities
+  
+  def create_kfi(self)
+  	puts "path etc"
+  end
+
 end
