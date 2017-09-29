3979ad531f5892534dcfd346cdb0719e70f89eb5
makerbox <mattwerth@mattwerth.com>
2017-09-29 12:57:03 +1000

    kfi test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,7 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		@order.last.kfi
+		Order.all.last.kfi
     redirect_to "http://roccloudy.com", alert: "order sent!"
   end
 end
