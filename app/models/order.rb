3dcac1f0e73f906e8851c6d7a9ce44ceaecab127
makerbox <mattwerth@mattwerth.com>
2017-10-12 20:02:00 +1100

    wrong ip

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,10 @@ class Order < ActiveRecord::Base
   has_many :products, through: :quantities
   
   def kfi
-  	filename = self.id.to_s self.id.to_s + self.user.account.company
+  	filename = self.id.to_s + self.user.account.company
   	path = "E:\\Attache\\Attache\\Roc\\KFIDATA\\Orders\\" + filename + ".kfi"
   	content = 'this is a test for the kfi file generator --- 
-  	"#{self.user.account.company}","","","","","","","#{filename}","","#{self.order_number}","","","",""
+  	"#{self.user.account.company}","","","","","","","#{filename}","","#{self.order_number.to_s}","","","",""
   	'
   	File.open(path, "w+") do |f|
   		f.write(content)
