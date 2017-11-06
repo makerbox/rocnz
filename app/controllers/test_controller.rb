cae5ed1935333cd27041b733914f339758f90c4e
makerbox <mattwerth@mattwerth.com>
2017-11-06 11:52:42 +1100

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,12 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		Order.all.last.kfi
-    redirect_to "http://roccloudy.com", alert: "order sent!"
-  end
+		results = []
+		dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
+        @products.each do |p|
+        	results << p.Inactive
+        end
+	end
+
 end
