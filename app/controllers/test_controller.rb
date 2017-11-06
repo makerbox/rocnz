2456d26ade9b86811d3a190a5cd112c9c43fa874
makerbox <mattwerth@mattwerth.com>
2017-11-06 13:22:17 +1100

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,14 +2,10 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-		results = []
+
 		dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
 		@products = dbh.execute("SELECT * FROM product_master").fetch(:all, :Struct)
-        @products.each do |p|
-        	results << p.Inactive
-        	results << p.Code
-        	results << '-----------'
-        end
+		dbh.disconnect
 	end
 
 end
