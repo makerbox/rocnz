568b204f4e466ad0fb80a7155971f581b2d94b2b
makerbox <mattwerth@mattwerth.com>
2018-01-15 12:44:03 +1100

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,27 +3,30 @@ class TestController < ApplicationController
 	
 	def index
 @results = []
-		dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
-      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-      @customers.each do |c|
-        code = c.Code.strip
-        if Account.all.find_by(code: code)
-          account = Account.all.find_by(code: code)
-          compname = c.Name
-          street = c.Street
-          suburb = c.Suburb 
-          state = c.Territory
-          postcode = c.Postcode 
-          phone = c.Phone 
-          sort = c.Sort 
-          discount = c.SpecialPriceCat 
-          seller_level = c.PriceCat
-          rep = c.SalesRep
-          account.update_attributes(approved: 'approved', phone: phone, street: street, state: state, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
-        @results << sort << '-----' << compname
-        end
-      end
-      dbh.disconnect 
+Account.all.each do |a|
+	@results << a.sort << '-------' << a.company
+end
+		# dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+  #     @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+  #     @customers.each do |c|
+  #       code = c.Code.strip
+  #       if Account.all.find_by(code: code)
+  #         account = Account.all.find_by(code: code)
+  #         compname = c.Name
+  #         street = c.Street
+  #         suburb = c.Suburb 
+  #         state = c.Territory
+  #         postcode = c.Postcode 
+  #         phone = c.Phone 
+  #         sort = c.Sort 
+  #         discount = c.SpecialPriceCat 
+  #         seller_level = c.PriceCat
+  #         rep = c.SalesRep
+  #         account.update_attributes(approved: 'approved', phone: phone, street: street, state: state, suburb: suburb, postcode: postcode, sort: sort, company: compname, rep: rep, seller_level: seller_level, discount: discount)
+  #       @results << sort << '-----' << compname
+  #       end
+  #     end
+  #     dbh.disconnect 
 	end
 
 end
