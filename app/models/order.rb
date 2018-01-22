a1de9d982dda9fb16a37b429d0650ad03c884b62
makerbox <mattwerth@mattwerth.com>
2018-01-22 13:50:47 +1100

    kfi no more

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,15 @@ class Order < ActiveRecord::Base
   has_many :products, through: :quantities
   
   def kfi
-  	filename = self.id.to_s + self.user.account.company
-  	path = "E:\\Attache\\Attache\\ROCNZ\\KFIDATA\\Orders\\" + filename + ".kfi"
-  	items = ''
-  	self.quantities.each do |q|
-  		product = q.product.code.to_s
-  		qty = q.qty.to_s
-  		items = items + '"'+product+'","'+qty+'","","","",""/r/n'
-  	end
-  	notes = self.notes
+  	# filename = self.id.to_s + self.user.account.company
+  	# path = "E:\\Attache\\Attache\\ROCNZ\\KFIDATA\\Orders\\" + filename + ".kfi"
+  	# items = ''
+  	# self.quantities.each do |q|
+  	# 	product = q.product.code.to_s
+  	# 	qty = q.qty.to_s
+  	# 	items = items + '"'+product+'","'+qty+'","","","",""/r/n'
+  	# end
+  	# notes = self.notes
   	# content = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+self.order_number.to_s+'","","","",""
   	# \r\n'+items+'<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes+'","","","","","","","","","","","","",""'
   	# File.open(path, "w+") do |f|
