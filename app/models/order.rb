976bb3c2f94a73b6c2ec03293d847d61c30a0e59
makerbox <mattwerth@mattwerth.com>
2018-02-26 16:25:17 +1100

    test

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,4 +27,26 @@ class Order < ActiveRecord::Base
     end
   end
 
+  def kfitest
+    filename = self.id.to_s + self.user.account.company
+    path = "E:\\Attache\\Attache\\ROCNZ\\KFIDATA\\Orders\\" + filename + ".kfi"
+    items = []
+    self.quantities.each do |q|
+      product = q.product.code.to_s
+      qty = q.qty.to_s
+      items << '"'+product+'","'+qty+'","","","",""'
+    end
+    notes1 = self.notes[0,60].to_s
+    len = self.notes.length
+    notes2 = self.notes[60,len].to_s
+    firstline = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""
+    '
+    lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes1+'","","'+notes2+'","","","","","","","","","","",""'
+      puts(firstline)
+      items.each do |i|
+        f.puts(i)
+      end
+      puts(lastline)
+  end
+
 end
