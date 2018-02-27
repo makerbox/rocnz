888275de291227058ece0d369c18a8c70a047de9
makerbox <mattwerth@mattwerth.com>
2018-02-27 14:50:12 +1100

    kfi works

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,8 +15,7 @@ class Order < ActiveRecord::Base
     notes1 = self.notes[0,60].to_s
     len = self.notes.length
     notes2 = self.notes[60,len].to_s
-    firstline = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""
-    '
+    firstline = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""'
     lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes1+'","","'+notes2+'","","","","","","","","","","",""'
     File.open(path, "w+") do |f|
       f.puts(firstline)
@@ -27,25 +26,6 @@ class Order < ActiveRecord::Base
     end
   end
 
-  def kfitest
-    filename = self.id.to_s + self.user.account.company
-    path = "E:\\Attache\\Attache\\ROCNZ\\KFIDATA\\Orders\\" + filename + ".kfi"
-    items = []
-    self.quantities.each do |q|
-      product = q.product.code.to_s
-      qty = q.qty.to_s
-      items << '"'+product+'","'+qty+'","","","",""'
-    end
-    notes1 = self.notes[0,60].to_s
-    len = self.notes.length
-    notes2 = self.notes[60,len].to_s
-    firstline = '"'+self.user.account.company+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""'
-    lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes1+'","","'+notes2+'","","","","","","","","","","",""'
-      puts(firstline)
-      items.each do |i|
-        puts(i)
-      end
-      puts(lastline)
-  end
+
 
 end
