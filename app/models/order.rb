a13165a43da8d31955a07f4ae6475bc85eb05a80
makerbox <mattwerth@mattwerth.com>
2018-03-16 13:36:32 +1100

    kfi fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,11 +12,11 @@ class Order < ActiveRecord::Base
       qty = q.qty.to_s
       items << '"'+product+'","'+qty+'","","","",""'
     end
-    notes1 = self.notes[0,60].to_s
-    len = self.notes.length
-    notes2 = self.notes[60,len].to_s
+    notes = self.notes.to_s
+    notes_first = notes[0..60].to_s
+    notes_second = notes[60..-1].to_s
     firstline = '"'+self.user.account.company.strip+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""'
-    lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes1+'","","'+notes2+'","","","","","","","","","","",""'
+    lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes_first+'","","'+notes_second+'","","","","","","","","","","",""'
     File.open(path, "w+") do |f|
       f.puts(firstline)
       items.each do |i|
