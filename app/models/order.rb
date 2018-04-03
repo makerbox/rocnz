7793339af50d06c19a7ea584c827d1953996006c
makerbox <mattwerth@mattwerth.com>
2018-04-03 15:27:12 +1000

    kfi adjustment

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,12 @@ class Order < ActiveRecord::Base
   has_many :products, through: :quantities
   
   def kfi
-    filename = self.id.to_s
+    filename = 'W' + self.id.to_s
     path = "E:\\Attache\\Attache\\ROCNZ\\KFIDATA\\Orders\\" + filename + ".kfi"
     items = []
-    self.quantities.each do |q|
+    myquantities = self.quantities.order(brand: :asc)
+    myquantities = myquantities.joins(:product).merge(Product.order(code: :asc)) #to order by associated product's code
+    myquantities.each do |q|
       product = q.product.code.to_s
       qty = q.qty.to_s
       items << '"'+product+'","'+qty+'","","","",""'
@@ -15,7 +17,7 @@ class Order < ActiveRecord::Base
     notes = self.notes.to_s
     notes_first = notes[0..60].to_s
     notes_second = notes[60..-1].to_s
-    firstline = '"'+self.user.account.company.strip+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""'
+    firstline = '"'+self.user.account.company.strip[0..7].to_s+'","","","","","","","'+filename+'","","'+Date.today.strftime('%d%m%Y').to_s+'","","","",""'
     lastline = '<F9><F4><DOWN><DOWN><DOWN><DOWN><ENTER>,"","","'+notes_first+'","","'+notes_second+'","","","","","","","","","","",""'
     File.open(path, "w+") do |f|
       f.puts(firstline)
@@ -25,7 +27,5 @@ class Order < ActiveRecord::Base
       f.puts(lastline)
     end
   end
-
-
 
 end
