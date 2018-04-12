0e7d84e43dcbb4aa86ac1db97a05227fcbc01974
makerbox <mattwerth@mattwerth.com>
2018-04-12 12:22:41 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,14 +2,15 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
+        @results = []
     dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
       @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
       @customers.each do |c|
         code = c.Code.strip
         if account = Account.all.find_by(code: code)
           # account = Account.all.find_by(code: code)
-          # @results << account.code
-          # account.update(dispute: c.InDispute)
+          @results << account.code
+          account.update(dispute: c.InDispute)
         end
       end
 	end
