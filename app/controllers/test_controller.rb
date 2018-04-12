9079df39207f230cb4509a1fbac754033498a00e
makerbox <mattwerth@mattwerth.com>
2018-04-12 12:37:54 +1000

    adf

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,18 +2,19 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-    #     @results = []
-    # dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
-    #   @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
-    #   @customers.each do |c|
-    #     code = c.Code.strip
-    #     if account = Account.all.find_by(code: code)
-    #       # account = Account.all.find_by(code: code)
-    #       @results << account.code
-    #       account.update(dispute: c.InDispute)
-    #     end
-    #   end
-    
+        @results = []
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+      @customers.each do |c|
+        code = c.Code.strip
+        if account = Account.all.find_by(code: code)
+            # account = Account.all.find_by(code: code)
+            if c.InDispute == 1
+                @results << account.code
+                account.update(dispute: true)
+            end
+        end
+      end
 	end
 
 end
