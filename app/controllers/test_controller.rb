56486dd29433ca616e16b61467c805615e28a7b6
makerbox <mattwerth@mattwerth.com>
2018-04-12 11:56:03 +1000

    test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,33 +2,16 @@ class TestController < ApplicationController
 	skip_before_action :authenticate_user!
 	
 	def index
-    @results = []
-    # Account.all.each do |a|
-    # 	@results << a.sort << '-------' << a.company
-    # end
-		def createrep(repemail, repcode)
-      if repuser = User.all.find_by(email: repemail)
-        repuser.add_role :rep
-        repuser.remove_role :user
-        if repuser.account
-          repuser.account.update_attributes(approved: 'approved', sort: 'U/L/R/P')
-        else
-          Account.create(code: repcode, company: 'Roc', user: repuser, sort: 'U/L/R/P')
+    dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
+      @customers = dbh.execute("SELECT * FROM customer_master").fetch(:all, :Struct)
+      @customers.each do |c|
+        code = c.Code.strip
+        if Account.all.find_by(code: code)
+          account = Account.all.find_by(code: code)
+          @results << account.code
+          account.update(dispute: c.InDispute)
         end
-      else
-        repuser = User.new(email: repemail, password:'cloudy_16', password_confirmation: 'cloudy_16')
-        repuser.add_role :rep
-        repuser.save(validate: false)
-        Account.create(code: repcode, company: 'Roc', user: repuser, sort: 'U/L/R/P')
       end
-    end
-
-    createrep('nsw@roccloudy.com', 'REPNSW')
-    createrep('vic@roccloudy.com', 'REPVIC')
-    createrep('qld1@roccloudy.com', 'REPQLD1')
-    createrep('qld2@roccloudy.com', 'REPQLD2')
-    createrep('nz@roccloudy.com', 'REPNZ')
-    createrep('office@roccloudy.com', 'ADMINOFFICE')
 	end
 
 end
