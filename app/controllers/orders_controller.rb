97d3c85ddff5eff4f2c8fd57f0788c9f9e94b715
makerbox <mattwerth@mattwerth.com>
2018-04-20 07:19:04 +1000

    asdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -72,14 +72,15 @@ end
   # GET /orders/1.json
   def show
     @sellerlevel = @order.user.account.seller_level
-    # if user_signed_in?
-    #   if ((current_user.has_role? :admin) || (current_user.has_role? :rep)) && (current_user.mimic) #for sidecart
-    #     @order = current_user.mimic.account.user.orders.where(active: true).last #for sidecart
-    #   else #for sidecart
-    #     @order = current_user.orders.where(active: true).last #for sidecart
-    #   end #for sidecart
-    # end
+
     if params[:showcart] == 'show'
+          if user_signed_in?
+      if ((current_user.has_role? :admin) || (current_user.has_role? :rep)) && (current_user.mimic) #for sidecart
+        @order = current_user.mimic.account.user.orders.where(active: true).last #for sidecart
+      else #for sidecart
+        @order = current_user.orders.where(active: true).last #for sidecart
+      end #for sidecart
+    end
         @showbuttons = 'show'
       else
         @showbuttons = 'noshow'
