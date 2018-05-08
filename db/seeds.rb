c74ea9e05ea7766737a9c58317f2d02f969ffd58
makerbox <mattwerth@mattwerth.com>
2018-05-08 13:14:02 +1000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -239,7 +239,9 @@ dbh = RDBI.connect :ODBC, :db => "wholesaleportalnz"
         when '6'
           payterms =  ce.TermsDays.to_s + 'Days after Month end'
         end
-        email = ce.EmailAddr.downcase
+        if !ce.EmailAddr.blank?
+          email = ce.EmailAddr.downcase
+        end
         if !Account.all.find_by(code: code)
           if email.blank?
             email = counter
